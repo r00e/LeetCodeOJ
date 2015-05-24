0505f28e6e7ad7817f76145592b5382391d88711
r00e <young_rui@hotmail.com>
2015-05-24 15:26:42 +0800

    longest substring without repeating characters solution2

---
+++ leetCode/src/03_LengthOfLongestSubstring/LengthOfLongestSubstringSolution2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace leetCode._03_LengthOfLongestSubstring
+{
+    class LengthOfLongestSubstringSolution2
+    {
+        private const int NO_REPEART = -1;
+
+        public int LengthOfLongestSubstring(string s)
+        {
+            var right = 0;
+            var left = 0;
+            var max = 0;
+
+            if (string.IsNullOrEmpty(s))
+            {
+                return 0;
+            }
+            while (right < s.Length && left <= right)
+            {
+                var repeateAt = RepeateAt(s, left, right);
+                if (repeateAt == NO_REPEART)
+                {
+                    right = right + 1;
+                }
+                else
+                {
+                    max = Math.Max(max, right - left);
+                    left = repeateAt + 1;
+                    right = right + 1;
+                }
+            }
+
+            max = Math.Max(max, right - left);
+
+            return max;
+        }
+
+        private static int RepeateAt(string s, int left, int right)
+        {
+            if (left == right)
+            {
+                return NO_REPEART;
+            }
+            while (left < right)
+            {
+                if (s[left] == s[right])
+                {
+                    return left;
+                }
+
+                left = left + 1;
+            }
+
+            return NO_REPEART;
+        }
+    }
+}
--- leetCode/test/LengthOfLongestSubstringFacts.cs
+++ leetCode/test/LengthOfLongestSubstringFacts.cs
@@ -7,77 +7,90 @@ namespace leetCode.test
     public class LengthOfLongestSubstringFacts
     {
         private readonly LengthOfLongestSubstringSolution solution = new LengthOfLongestSubstringSolution();
+        private readonly LengthOfLongestSubstringSolution2 solution2 = new LengthOfLongestSubstringSolution2();
 
         [Fact]
         public void should_return_0_when_input_empty_string()
         {
             solution.LengthOfLongestSubstring(string.Empty).ShouldBe(0);
+            solution2.LengthOfLongestSubstring(string.Empty).ShouldBe(0);
         }
 
         [Fact]
         public void should_return_1_when_input_a()
         {
             solution.LengthOfLongestSubstring("a").ShouldBe(1);
+            solution2.LengthOfLongestSubstring("a").ShouldBe(1);
         }
 
         [Fact]
         public void should_return_1_when_input_aa()
         {
             solution.LengthOfLongestSubstring("aa").ShouldBe(1);
+            solution2.LengthOfLongestSubstring("aa").ShouldBe(1);
         }
 
         [Fact]
         public void should_return_2_when_input_ab()
         {
             solution.LengthOfLongestSubstring("ab").ShouldBe(2);
+            solution2.LengthOfLongestSubstring("ab").ShouldBe(2);
         }
 
         [Fact]
         public void should_return_2_when_input_aba()
         {
             solution.LengthOfLongestSubstring("aba").ShouldBe(2);
+            solution2.LengthOfLongestSubstring("aba").ShouldBe(2);
         }
 
         [Fact]
         public void should_return_2_when_input_aaaaaaaaaba()
         {
             solution.LengthOfLongestSubstring("aaaaaaaaaba").ShouldBe(2);
+            solution2.LengthOfLongestSubstring("aaaaaaaaaba").ShouldBe(2);
         }
 
         [Fact]
         public void should_return_3_when_input_aaaaaaaaabac()
         {
             solution.LengthOfLongestSubstring("aaaaaaaaabac").ShouldBe(3);
+            solution2.LengthOfLongestSubstring("aaaaaaaaabac").ShouldBe(3);
         }
 
         [Fact]
         public void should_return_4_when_input_aababcdc()
         {
             solution.LengthOfLongestSubstring("aababcdc").ShouldBe(4);
+            solution2.LengthOfLongestSubstring("aababcdc").ShouldBe(4);
         }
 
         [Fact]
         public void should_return_2_when_input_abba()
         {
             solution.LengthOfLongestSubstring("abba").ShouldBe(2);
+            solution2.LengthOfLongestSubstring("abba").ShouldBe(2);
         }
 
         [Fact]
         public void should_return_3_when_input_abbac()
         {
             solution.LengthOfLongestSubstring("abbac").ShouldBe(3);
+            solution2.LengthOfLongestSubstring("abbac").ShouldBe(3);
         }
 
         [Fact]
         public void should_return_3_when_input_abcbac()
         {
             solution.LengthOfLongestSubstring("abcbac").ShouldBe(3);
+            solution2.LengthOfLongestSubstring("abcbac").ShouldBe(3);
         }
 
         [Fact]
         public void should_return_3_when_input_abcbadea()
         {
             solution.LengthOfLongestSubstring("abcbadea").ShouldBe(5);
+            solution2.LengthOfLongestSubstring("abcbadea").ShouldBe(5);
         }
     }
 }
