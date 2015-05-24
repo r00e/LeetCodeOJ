bb213c490680bf76230b3b28f81331a20169c67e
r00e <young_rui@hotmail.com>
2015-05-24 11:11:54 +0800

    length of longest substring: empty input and 1 character string

---
+++ leetCode/src/03_LengthOfLongestSubstring/LengthOfLongestSubstringSolution.cs
@@ -0,0 +1,15 @@
+﻿namespace leetCode._03_LengthOfLongestSubstring
+{
+    class LengthOfLongestSubstringSolution
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return 0;
+            }
+
+            return s.Length;
+        }
+    }
+}
---
+++ leetCode/test/LengthOfLongestSubstringFacts.cs
@@ -0,0 +1,23 @@
+﻿using leetCode._03_LengthOfLongestSubstring;
+using Xunit;
+using Xunit.Should;
+
+namespace leetCode.test
+{
+    public class LengthOfLongestSubstringFacts
+    {
+        private readonly LengthOfLongestSubstringSolution solution = new LengthOfLongestSubstringSolution();
+
+        [Fact]
+        public void should_return_0_when_input_empty_string()
+        {
+            solution.LengthOfLongestSubstring(string.Empty).ShouldBe(0);
+        }
+
+        [Fact]
+        public void should_return_1_when_input_a()
+        {
+            solution.LengthOfLongestSubstring("a").ShouldBe(1);
+        }
+    }
+}
