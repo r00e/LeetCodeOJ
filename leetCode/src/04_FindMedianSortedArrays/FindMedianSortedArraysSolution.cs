f1e9c4a5fffc943a8ed20f10ee0d170d0a87f7e3
r00e <young_rui@hotmail.com>
2015-05-24 15:57:25 +0800

    find median sorted arrays

---
+++ leetCode/src/04_FindMedianSortedArrays/FindMedianSortedArraysSolution.cs
@@ -0,0 +1,10 @@
+﻿namespace leetCode._04_FindMedianSortedArrays
+{
+    public class FindMedianSortedArraysSolution
+    {
+        public double FindMedianSortedArrays(int[] array1, int[] array2)
+        {
+            return 0;
+        }
+    }
+}
---
+++ leetCode/test/FindMedianSortedArraysFacts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using leetCode._04_FindMedianSortedArrays;
+using Xunit;
+
+namespace leetCode.test
+{
+    public class FindMedianSortedArraysFacts
+    {
+        private readonly FindMedianSortedArraysSolution solution = new FindMedianSortedArraysSolution();
+
+        [Fact]
+        public void should_return_0_when_arrays_are_0_and_0()
+        {
+            solution.FindMedianSortedArrays(new int[] {0}, new int[] {0});
+        }
+    }
+}
