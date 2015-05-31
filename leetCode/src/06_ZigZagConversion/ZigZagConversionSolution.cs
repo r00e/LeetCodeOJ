d8c678a830943f75799fd071979a89d30864b290
r00e <young_rui@hotmail.com>
2015-05-31 23:03:50 +0800

    06_ZigZagConversion

---
+++ leetCode/src/06_ZigZagConversion/ZigZagConversionSolution.cs
@@ -0,0 +1,10 @@
+﻿namespace leetCode._06_ZigZagConversion
+{
+    public class ZigZagConversionSolution
+    {
+        public string Convert(string text, int nRows)
+        {
+            return text;
+        }
+    }
+}
---
+++ leetCode/test/ZigZagConversionFacts.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.InteropServices;
+using leetCode._06_ZigZagConversion;
+using Xunit;
+using Xunit.Extensions;
+using Xunit.Should;
+
+namespace leetCode.test
+{
+    public class ZigZagConversionFacts
+    {
+        private ZigZagConversionSolution solution = new ZigZagConversionSolution();
+
+        [Theory]
+        [InlineData("P", 1, "P")]
+        public void should_return_in_zigzag_pattern(string text, int nRows, string expected)
+        {
+            solution.Convert(text, nRows).ShouldBe(expected);
+        }
+    }
+}
