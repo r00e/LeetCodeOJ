86006dc8fbaadb9bcde9d2e0d3fd511b578b94bc
r00e <young_rui@hotmail.com>
2015-05-31 12:35:37 +0800

    ugly FindKth

--- leetCode/src/04_FindMedianSortedArrays/FindMedianSortedArraysSolution.cs
+++ leetCode/src/04_FindMedianSortedArrays/FindMedianSortedArraysSolution.cs
@@ -1,23 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Runtime.InteropServices;
+﻿// http://articles.leetcode.com/2011/01/find-k-th-smallest-element-in-union-of.html
+
+using System;
 
 namespace leetCode._04_FindMedianSortedArrays
 {
     class FindMedianSortedArraysSolution
     {
-        public double FindMedianSortedArrays(int[] array1, int[] array2)
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
         {
-            var middle = (array1.Length + array2.Length)/2;
-            if ((array1.Length + array2.Length)%2 != 0)
+            int len1 = nums1.Length, len2 = nums2.Length;
+            var totalLen = len1 + len2;
+            var midIndex = (len1 + len2) / 2;
+            var mid = FindKth(nums1, 0, len1 - 1, nums2, 0, len2 - 1, midIndex);
+
+            if (totalLen % 2 != 0)
             {
-                return array1.Length > middle ? array1[middle] : array2[middle - array1.Length];
+                return mid;
             }
             else
             {
-                return (double)(array1[0] + array2[0])/2;
+                mid = mid + FindKth(nums1, 0, len1-1, nums2, 0, len2-1, midIndex - 1);
+                mid = mid / 2;
             }
+
+            return mid;
+        }
+
+        private double FindKth(int[] nums1, int l1, int r1, int[] nums2, int l2, int r2, int k)
+        {
+            var len1 = r1 - l1 + 1;
+            var len2 = r2 - l2 + 1;
+
+            if (len1 == 0) return nums2[l2 + k];
+            if (len2 == 0) return nums1[l1 + k];
+            if (k == 0) return Math.Min(nums1[l1], nums2[l2]);
+
+            var i = k*(len1)/(len1 + len2);
+            var j = k - i;
+
+            var numPreI = i <= 0 ? int.MinValue : nums1[l1 + i - 1];
+            var numPreJ = j <=0 ? int.MinValue : nums2[l2 + j - 1];
+            var numI = i == len1 ? int.MaxValue : nums1[l1 + i];
+            var numJ = j == len2 ? int.MaxValue : nums2[l2 + j];
+
+            if (numPreJ <= numI && numI <= numJ) return numI;
+            if (numPreI <= numJ && numJ <= numI) return numJ;
+
+            if (numI <= numJ) return FindKth(nums1, l1 + i + 1, r1, nums2, l2, (l2 + j - 1) < l2 ? l2 - 1 : j - 1, k - i - 1);
+            else return FindKth(nums1, l1, (l1 + i - 1) < l1 ? l1 - 1 : i - 1, nums2, l2 + j + 1, r2, k - j - 1);
         }
     }
 }
--- leetCode/test/FindMedianSortedArraysFacts.cs
+++ leetCode/test/FindMedianSortedArraysFacts.cs
@@ -1,4 +1,5 @@
-﻿using leetCode._04_FindMedianSortedArrays;
+﻿using System;
+using leetCode._04_FindMedianSortedArrays;
 using Xunit.Extensions;
 using Xunit.Should;
 
@@ -9,9 +10,21 @@ namespace leetCode.test
         private readonly FindMedianSortedArraysSolution solution = new FindMedianSortedArraysSolution();
 
         [Theory]
+        [InlineData(new int[] {0}, new int[] {}, 0)]
+        [InlineData(new int[] {}, new int[] {0}, 0)]
+        [InlineData(new int[] {0, 1}, new int[] {}, 0.5)]
+        [InlineData(new int[] {}, new int[] {0, 1}, 0.5)]
         [InlineData(new int[] {0}, new int[] {0}, 0)]
         [InlineData(new int[] {0}, new int[] {1}, 0.5)]
-        [InlineData(new int[] {0}, new int[] {1, 1}, 1)]
+        [InlineData(new int[] {0}, new int[] {1, 2}, 1)]
+        [InlineData(new int[] {1, 2}, new int[] { 0 }, 1)]
+        [InlineData(new int[] {1, 2, 3}, new int[] {0}, 1.5)]
+        [InlineData(new int[] {1, 3}, new int[] {2, 4}, 2.5)]
+        [InlineData(new int[] {1, 3}, new int[] {2, 4, 5}, 3)]
+        [InlineData(new int[] {1, 1}, new int[] {1, 1}, 1)]
+        [InlineData(new int[] {1, 2}, new int[] {2, 3}, 2)]
+        [InlineData(new int[] {1, 2, 3}, new int[] {2, 3}, 2)]
+        [InlineData(new int[] {1, 6, 7, 8, 9}, new int[] {2, 3, 4, 5, 10}, 5.5)]
         public void should_return_median(int[] array1, int[] array2, double median)
         {
             solution.FindMedianSortedArrays(array1, array2).ShouldBe(median);
