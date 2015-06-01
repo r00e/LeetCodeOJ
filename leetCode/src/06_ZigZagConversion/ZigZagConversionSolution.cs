6229fa773d9604ef9f44d25b61fa20a30a656a37
r00e <young_rui@hotmail.com>
2015-06-01 21:41:48 +0800

    zig zag

--- leetCode/src/06_ZigZagConversion/ZigZagConversionSolution.cs
+++ leetCode/src/06_ZigZagConversion/ZigZagConversionSolution.cs
@@ -1,10 +1,45 @@
-﻿namespace leetCode._06_ZigZagConversion
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace leetCode._06_ZigZagConversion
 {
     public class ZigZagConversionSolution
     {
-        public string Convert(string text, int nRows)
+        public string Convert(string s, int numRows)
         {
-            return text;
+            if (numRows == 1) return s;
+
+            var result = "";
+
+            for (var currentLine = 0; currentLine < numRows; currentLine++)
+            {
+                var isFirstOrLastLine = currentLine == 0 || currentLine == numRows - 1 ? true : false;
+                var index = currentLine;
+                
+                while (index < s.Length)
+                {
+                    if (isFirstOrLastLine)
+                    {
+                        result += s[index];
+                        index += 2 * (numRows - 1);
+                    }
+                    else
+                    {
+                        result += s[index];
+                        index += GetOffset(currentLine, numRows);
+                        if (index >= s.Length) continue;
+                        result += s[index];
+                        index += 2 * currentLine;
+                    }
+                }
+            }
+            return result;
+        }
+
+        private int GetOffset(int currentLine, int numRows)
+        {
+            return 2*(numRows - currentLine - 1);
         }
     }
 }
--- leetCode/test/ZigZagConversionFacts.cs
+++ leetCode/test/ZigZagConversionFacts.cs
@@ -12,6 +12,11 @@ namespace leetCode.test
 
         [Theory]
         [InlineData("P", 1, "P")]
+        [InlineData("PA", 1, "PA")]
+        [InlineData("PA", 2, "PA")]
+        [InlineData("PAY", 2, "PYA")]
+        [InlineData("PAYP", 3, "PAPY")]
+        [InlineData("PAYPALISHIRING", 3, "PAHNAPLSIIGYIR")]
         public void should_return_in_zigzag_pattern(string text, int nRows, string expected)
         {
             solution.Convert(text, nRows).ShouldBe(expected);
