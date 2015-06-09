6ea83cc197bb360b542ade29e4a624dd47d87958
r00e <young_rui@hotmail.com>
2015-06-09 20:39:12 +0800

    remove nth node from end of list

---
+++ leetCode/src/019_RemoveNthNodeFromEndOfListSolution/RemoveNthNodeFromEndOfListSolution.cs
@@ -0,0 +1,30 @@
+﻿namespace leetCode._019_RemoveNthNodeFromEndOfListSolution
+{
+    public class RemoveNthNodeFromEndOfListSolution
+    {
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            var length = 0;
+            var tmp = head;
+
+            while (tmp != null)
+            {
+                length = length + 1;
+                tmp = tmp.next;
+            }
+
+            if (n == length) return head.next;
+
+            tmp = head;
+            var count = length - n - 1;
+            while (count > 0)
+            {
+                tmp = tmp.next;
+                count--;
+            }
+            tmp.next = tmp.next.next;
+
+            return head;
+        }
+    }
+}
---
+++ leetCode/src/019_RemoveNthNodeFromEndOfListSolution/RemoveNthNodeFromEndOfListSolution2.cs
@@ -0,0 +1,30 @@
+﻿namespace leetCode._019_RemoveNthNodeFromEndOfListSolution
+{
+    public class RemoveNthNodeFromEndOfListSolution2
+    {
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            var tmpHead = new ListNode(0);
+            tmpHead.next = head;
+
+            var front = tmpHead;
+            var back = tmpHead;
+
+            while (n > 0)
+            {
+                front = front.next;
+                n--;
+            }
+
+            while (front.next != null)
+            {
+                front = front.next;
+                back = back.next;
+            }
+
+            back.next = back.next.next;
+
+            return tmpHead.next;
+        }
+    }
+}
