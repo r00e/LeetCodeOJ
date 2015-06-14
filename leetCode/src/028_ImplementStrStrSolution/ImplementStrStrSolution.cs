0a519e58daf68b08df0f1956d7b6658b5a795cfe
r00e <young_rui@hotmail.com>
2015-06-14 17:04:15 +0800

    implement strStr using KMP

---
+++ leetCode/src/028_ImplementStrStrSolution/ImplementStrStrSolution.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+
+namespace leetCode._028_ImplementStrStrSolution
+{
+    public class ImplementStrStrSolution
+    {
+        // KMP
+        public int StrStr(string haystack, string needle)
+        {
+            if (haystack.Length < needle.Length) return -1;
+            if (haystack.Length == 0 && needle.Length == 0) return 0;
+            if (needle.Length == 0) return 0;
+
+            var kmpHelper = KmpHelper(needle);
+            var needleIndex = -1;
+            for (var i = 0; i < haystack.Length; i++)
+            {
+                while (needleIndex > -1 && haystack[i] != needle[needleIndex + 1])
+                {
+                    needleIndex = kmpHelper[needleIndex];
+                }
+
+                if(needle[needleIndex + 1] == haystack[i]) needleIndex++;
+                if (needleIndex == needle.Length - 1) return i - needle.Length + 1;
+            }
+
+            return -1;
+        }
+
+        private Dictionary<int, int> KmpHelper(string str)
+        {
+            var dict = new Dictionary<int, int> {{0, -1}};
+            var kmpIndex = -1;
+
+            for (var i = 1; i < str.Length; i++)
+            {
+                while (kmpIndex > -1 && str[i] != str[kmpIndex+1])
+                {
+                    kmpIndex = dict[kmpIndex];
+                }
+
+                if (str[i] == str[kmpIndex+1])
+                {
+                    kmpIndex = kmpIndex + 1;
+                }
+
+                dict.Add(i, kmpIndex);
+            }
+
+            return dict;
+        }
+    }
+}
---
+++ leetCode/test/ImplementStrStrFacts.cs
@@ -0,0 +1,26 @@
+﻿using leetCode._028_ImplementStrStrSolution;
+using Xunit.Extensions;
+using Xunit.Should;
+
+namespace leetCode.test
+{
+    public class ImplementStrStrFacts
+    {
+        private readonly ImplementStrStrSolution solution = new ImplementStrStrSolution();
+
+        [Theory]
+        [InlineData("", "", 0)]
+        [InlineData("a", "", 0)]
+        [InlineData("a", "b", -1)]
+        [InlineData("a", "a", 0)]
+        [InlineData("aaa", "aa", 0)]
+        [InlineData("cab", "ab", 1)]
+        [InlineData("cdefgab", "ab", 5)]
+        [InlineData("mississippi", "issip", 4)]
+        
+        public void should_return_index_of_the_first_occurence_of_needle_in_haystack(string haystack, string needle, int expectIndex)
+        {
+            solution.StrStr(haystack, needle).ShouldBe(expectIndex);
+        }
+    }
+}
