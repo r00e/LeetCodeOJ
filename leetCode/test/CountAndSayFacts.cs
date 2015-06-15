6cb30205f1f5d2c3bb5c8319f195a5c22eca3786
r00e <young_rui@hotmail.com>
2015-06-15 21:39:41 +0800

    return specified string for input integer

---
+++ leetCode/test/CountAndSayFacts.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using leetCode._036_ValidSudokuSolution;
+using Xunit;
+using Xunit.Extensions;
+using Xunit.Should;
+
+namespace leetCode.test
+{
+    public class CountAndSayFacts
+    {
+        private readonly CountAndSaySolution solution = new CountAndSaySolution();
+
+        [Theory]
+        [InlineData(1, "1")]
+        [InlineData(2, "2")]
+        public void should_return_integer(int num, string expectedValue)
+        {
+            solution.CountAndSay(num).ShouldBe(expectedValue);
+        }
+    }
+
+    internal class CountAndSaySolution
+    {
+        public string CountAndSay(int n)
+        {
+            var dict = new Dictionary<int, string>
+            {
+                {1, "1"}, {2, "2"}, {3, "3"}, {4, "4"}, {5, "5"},
+                {6, "6"}, {7, "7"}, {8, "8"}, {9, "9"}, {0, "0"}
+            };
+            return dict[n];
+        }
+    }
+}
