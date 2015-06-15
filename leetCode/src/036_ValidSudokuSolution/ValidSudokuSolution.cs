5fbe70b9b91ec41d1dd1a6e1a6424432c460f8ab
r00e <young_rui@hotmail.com>
2015-06-15 19:30:15 +0800

    valid sudoku

---
+++ leetCode/src/036_ValidSudokuSolution/ValidSudokuSolution.cs
@@ -0,0 +1,46 @@
+﻿namespace leetCode._036_ValidSudokuSolution
+{
+    public class ValidSudokuSolution
+    {
+        public bool IsValidSudoku(char[,] board)
+        {
+            for (var i = 0; i <= board.GetUpperBound(0); i++)
+            {
+                for (var j = 0; j <= board.GetUpperBound(1); j++)
+                {
+                    if(board[i, j] == '.') continue;
+
+                    if(!ValidateNum(board[i, j], i, j, board)) return false;
+                }
+            }
+
+            return true;
+        }
+
+        private bool ValidateNum(char c, int x, int y, char[,] board)
+        {
+            // validate the column
+            for (var i = 0; i <= board.GetUpperBound(0); i++)
+            {
+                if(board[i, y] == c && i != x) return false;
+            }
+
+            // validate the row
+            for (var j = 0; j <= board.GetUpperBound(1); j++)
+            {
+                if(board[x, j] == c && j != y) return false;
+            }
+
+            // validate the block
+            for (var xIndex = x/3*3; xIndex < x/3*3 + 3; xIndex++)
+            {
+                for (var yIndex = y/3*3; yIndex < y/3*3 + 3; yIndex++)
+                {
+                    if (board[xIndex, yIndex] == c && (xIndex != x || yIndex != y)) return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ leetCode/test/ValidSudokuFacts.cs
@@ -0,0 +1,30 @@
+﻿using leetCode._036_ValidSudokuSolution;
+using Xunit;
+using Xunit.Should;
+
+namespace leetCode.test
+{
+    public class ValidSudokuFacts
+    {
+        private readonly ValidSudokuSolution solution = new ValidSudokuSolution();
+
+        [Fact]
+        public void should_return_integer()
+        {
+            var testBoard = new char[9,9]
+            {
+                {'.', '.', '4', '.', '.', '.', '6', '3', '.'},
+                {'.', '.', '.', '.', '.', '.', '.', '.', '.'},
+                {'5', '.', '.', '.', '.', '.', '.', '9', '.'},
+                {'.', '.', '.', '5', '6', '.', '.', '.', '.'},
+                {'4', '.', '3', '.', '.', '.', '.', '.', '1'},
+                {'.', '.', '.', '7', '.', '.', '.', '.', '.'},
+                {'.', '.', '.', '5', '.', '.', '.', '.', '.'},
+                {'.', '.', '.', '.', '.', '.', '.', '.', '.'},
+                {'.', '.', '.', '.', '.', '.', '.', '.', '.'}
+            };
+
+            solution.IsValidSudoku(testBoard).ShouldBe(false);
+        }
+    }
+}
