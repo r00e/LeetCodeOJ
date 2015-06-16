4ce1737caced164c98f09428a8079c90195b28b3
r00e <young_rui@hotmail.com>
2015-06-16 21:55:52 +0800

    length of last word

---
+++ leetCode/src/058_LengthOfLastWordSolution/LengthOfLastWordSolution.cs
@@ -0,0 +1,38 @@
+﻿namespace leetCode._058_LengthOfLastWordSolution
+{
+    public class LengthOfLastWordSolution
+    {
+        public int LengthOfLastWord(string s)
+        {
+            return s.Trim().Length - s.Trim().LastIndexOf(' ') - 1;
+        }
+
+        public int LengthOfLastWord2(string s)
+        {
+            s = s.Trim();
+
+            for (var i = 0; i < s.Length; i++)
+            {
+                if (s[s.Length - 1 - i] == ' ') return i;
+                if (i == s.Length - 1) return i + 1;
+            }
+
+            return 0;
+        }
+
+        public int LengthOfLastWord3(string s)
+        {
+            if (s.Length == 0) return 0;
+            var lastNonSpace = 0;
+            var lastSeperateSpace = -1;
+            var index = s.Length - 1;
+
+            while (index >= 0 && s[index] == ' ') index--;
+            lastNonSpace = index;
+            while (index >= 0 && s[index] != ' ') index--;
+            lastSeperateSpace = index;
+
+            return lastNonSpace - lastSeperateSpace;
+        }
+    }
+}
---
+++ leetCode/test/LengthOfLastWordFacts.cs
@@ -0,0 +1,23 @@
+﻿using leetCode._058_LengthOfLastWordSolution;
+using Xunit.Extensions;
+using Xunit.Should;
+
+namespace leetCode.test
+{
+    public class LengthOfLastWordFacts
+    {
+        private readonly LengthOfLastWordSolution solution = new LengthOfLastWordSolution();
+
+        [Theory]
+        [InlineData("", 0)]
+        [InlineData("a", 1)]
+        [InlineData("a ", 1)]
+        [InlineData("a bc", 2)]
+        public void should_return_integer(string s, int expectedValue)
+        {
+            solution.LengthOfLastWord(s).ShouldBe(expectedValue);
+            solution.LengthOfLastWord2(s).ShouldBe(expectedValue);
+            solution.LengthOfLastWord3(s).ShouldBe(expectedValue);
+        }
+    }
+}
