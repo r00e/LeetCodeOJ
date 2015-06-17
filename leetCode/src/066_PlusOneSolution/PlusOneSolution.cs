67aa5a818053727c784d9527c107ddbfa769347e
r00e <young_rui@hotmail.com>
2015-06-17 21:06:40 +0800

    update the plus one method

--- leetCode/src/066_PlusOneSolution/PlusOneSolution.cs
+++ leetCode/src/066_PlusOneSolution/PlusOneSolution.cs
@@ -22,10 +22,14 @@ namespace leetCode._066_PlusOneSolution
             var result = new int[digits.Length + 1];
             result[0] = 1;
 
-            for (var j = 1; j < digits.Length; j++)
-            {
-                result[j] = digits[j];
-            }
+            // in the "digits"
+            // if the highest digit is 10, it indicates that all the lower digit are 9, like below:
+            // digits: 9 9 9, after it is processed by the code above, it looks like
+            // processed digits:  10 0 0
+            // initial result:     1 0 0 0
+            // This is why we just have to initialize an new array with the 1st element is 1 and all the
+            // others are 0,
+            // we don't have to copy the digits anymore.
 
             return result;
         }
