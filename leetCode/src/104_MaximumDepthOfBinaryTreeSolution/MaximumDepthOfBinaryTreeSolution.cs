856b3e4e4c6b4e92e9c1ef6ebd0ca926744efb5b
r00e <young_rui@hotmail.com>
2015-06-26 00:21:14 +0800

    maximum depth of binary tree

---
+++ leetCode/src/104_MaximumDepthOfBinaryTreeSolution/MaximumDepthOfBinaryTreeSolution.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using leetCode.Common;
+
+namespace leetCode._104_MaximumDepthOfBinaryTreeSolution
+{
+    public class MaximumDepthOfBinaryTreeSolution
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            if(root == null) return 0;
+
+            var queue = new Queue<TreeNode>();
+            queue.Enqueue(root);
+            var allNodesByLevel = new List<IList<int>>();
+
+            while (queue.Count != 0)
+            {
+                var count = queue.Count;
+                for (var i = 0; i < count; i++)
+                {
+                    if(queue.Peek().left != null) queue.Enqueue(queue.Peek().left);
+                    if(queue.Peek().right != null) queue.Enqueue(queue.Peek().right);
+
+                    if(i == 0) allNodesByLevel.Add(new List<int>(){queue.Dequeue().val});
+                    else allNodesByLevel.Last().Add(queue.Dequeue().val);
+                }
+            }
+
+            return allNodesByLevel.Count;
+        }
+    }
+}
---
+++ leetCode/test/MaximumDepthOfBinaryTreeFacts.cs
@@ -0,0 +1,49 @@
+﻿using leetCode.Common;
+using leetCode._104_MaximumDepthOfBinaryTreeSolution;
+using Xunit;
+using Xunit.Should;
+
+namespace leetCode.test
+{
+    public class MaximumDepthOfBinaryTreeFacts
+    {
+        private readonly MaximumDepthOfBinaryTreeSolution solution = new MaximumDepthOfBinaryTreeSolution();
+
+        [Fact]
+        public void should_return_empty_when_tree_is_null()
+        {
+            solution.MaxDepth(null).ShouldBe(0);
+        }
+
+        [Fact]
+        public void should_return_1_when_tree_is_0()
+        {
+            var tree = new TreeNode(0);
+            solution.MaxDepth(tree).ShouldBe(1);
+        }
+
+        [Fact]
+        public void should_return_2_when_tree_is_01()
+        {
+            var tree = new TreeNode(0){left = new TreeNode(1)};
+            solution.MaxDepth(tree).ShouldBe(2);
+        }
+
+        [Fact]
+        public void should_return_2_when_tree_is_0_1()
+        {
+            var tree = new TreeNode(0){right = new TreeNode(1)};
+            solution.MaxDepth(tree).ShouldBe(2);
+        }
+
+        [Fact]
+        public void should_return_3_when_tree_is_0_1_2()
+        {
+            var tree = new TreeNode(0)
+            {
+                right = new TreeNode(1) { right = new TreeNode(2)}
+            };
+            solution.MaxDepth(tree).ShouldBe(3);
+        }
+    }
+}
