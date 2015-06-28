36cc2a47a4a5a6f0308bf10f69caf511e2660e97
r00e <young_rui@hotmail.com>
2015-06-28 19:10:49 +0800

    valid palindrome

---
+++ leetCode/src/125_ValidPalindromeSolution/ValidPalindromeSolution.cs
@@ -0,0 +1,45 @@
+﻿namespace leetCode._125_ValidPalindromeSolution
+{
+    public class ValidPalindromeSolution
+    {
+        public bool IsPalindrome(string s)
+        {
+            if(string.IsNullOrEmpty(s)) return true;
+
+            var endCharIndex = s.Length - 1;
+            while (!IsValidChar(s[endCharIndex]))
+            {
+                endCharIndex--;
+                if (endCharIndex < 0) return true;
+            }
+
+            for (var i = 0; i < endCharIndex; i++)
+            {
+                while (!IsValidChar(s[i]))
+                {
+                    i++;
+                }
+
+                if (char.ToLower(s[i]) != char.ToLower(s[endCharIndex]))
+                {
+                    return false;
+                }
+                else if(i != endCharIndex)
+                {
+                    endCharIndex--;
+                    while (!IsValidChar(s[endCharIndex]))
+                    {
+                        endCharIndex--;
+                    }
+                }
+            }
+            return true;
+        }
+
+        private static bool IsValidChar(char c)
+        {
+            c = char.ToLower(c);
+            return (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9');
+        }
+    }
+}
---
+++ leetCode/test/ValidPalindromeFacts.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using leetCode._119_PascalTriangleIISolution;
+using leetCode._125_ValidPalindromeSolution;
+using Xunit;
+using Xunit.Extensions;
+using Xunit.Should;
+
+namespace leetCode.test
+{
+    public class ValidPalindromeFacts
+    {
+        private readonly ValidPalindromeSolution solution = new ValidPalindromeSolution();
+
+        [Theory]
+        [InlineData("", true)]
+        [InlineData(" ", true)]
+        [InlineData("a", true)]
+        [InlineData("ab", false)]
+        [InlineData("aba", true)]
+        [InlineData("a.", true)]
+        [InlineData(".a", true)]
+        [InlineData("a a.", true)]
+        [InlineData("a A.", true)]
+        [InlineData("A man, a plan, a canal: Panama", true)]
+        [InlineData("1a, 1", true)]
+        [InlineData("1a, 2", false)]
+        public void should_return_result_for_string(string input, bool expectedValue)
+        {
+            solution.IsPalindrome(input).ShouldBe(expectedValue);
+        }
+    }
+}
