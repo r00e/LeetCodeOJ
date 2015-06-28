8dda2c01e01c2486f1418746814f62835a72ea72
r00e <young_rui@hotmail.com>
2015-06-28 13:31:59 +0800

    pascal triangle II

---
+++ leetCode/src/119_PascalTriangleIISolution/PascalTriangleSolutionII.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace leetCode._119_PascalTriangleIISolution
+{
+    public class PascalTriangleSolutionII
+    {
+        public IList<int> GetRow(int rowIndex)
+        {
+            var length = rowIndex + 1;
+            var result = new int[length];
+
+            for (var i = 0; i < length; i++)
+            {
+                result[i] = 1;
+
+                for (var j = i - 1; j >= 1; j--)
+                {
+                    result[j] = result[j - 1] + result[j];
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ leetCode/test/PascalTriangleIIFacts.cs
@@ -0,0 +1,38 @@
+﻿using leetCode._119_PascalTriangleIISolution;
+using Xunit;
+using Xunit.Should;
+
+namespace leetCode.test
+{
+    public class PascalTriangleIIFacts
+    {
+        private readonly PascalTriangleSolutionII solution = new PascalTriangleSolutionII();
+
+        [Fact]
+        public void should_return_1_when_input_is_0()
+        {
+            var result = solution.GetRow(0);
+            result.Count.ShouldBe(1);
+            result[0].ShouldBe(1);
+        }
+
+        [Fact]
+        public void should_return_11_when_input_is_1()
+        {
+            var result = solution.GetRow(1);
+            result.Count.ShouldBe(2);
+            result[0].ShouldBe(1);
+            result[1].ShouldBe(1);
+        }
+
+        [Fact]
+        public void should_return_121_when_input_is_2()
+        {
+            var result = solution.GetRow(2);
+            result.Count.ShouldBe(3);
+            result[0].ShouldBe(1);
+            result[1].ShouldBe(2);
+            result[2].ShouldBe(1);
+        }
+    }
+}
