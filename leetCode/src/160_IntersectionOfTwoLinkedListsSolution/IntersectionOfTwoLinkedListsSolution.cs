393f7966d168a5182278549693a2bdf7ec517fd2
r00e <young_rui@hotmail.com>
2015-06-30 22:47:10 +0800

    intersection of two linked lists

---
+++ leetCode/src/160_IntersectionOfTwoLinkedListsSolution/IntersectionOfTwoLinkedListsSolution.cs
@@ -0,0 +1,20 @@
+﻿using leetCode.Common;
+
+namespace leetCode._160_IntersectionOfTwoLinkedListsSolution
+{
+    public class IntersectionOfTwoLinkedListsSolution
+    {
+        public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+        {
+            if(headA == null || headB == null) return null;
+            var hA = headA;
+            var hB = headB;
+            while (hA != hB)
+            {
+                hA = hA == null ? headB : hA.next;
+                hB = hB == null ? headA : hB.next;
+            }
+            return hA;
+        }
+    }
+}
---
+++ leetCode/test/IntersectionOfTwoLinkedListsFacts.cs
@@ -0,0 +1,66 @@
+﻿using leetCode.Common;
+using leetCode._160_IntersectionOfTwoLinkedListsSolution;
+using Xunit;
+using Xunit.Should;
+
+namespace leetCode.test
+{
+    public class IntersectionOfTwoLinkedListsFacts
+    {
+        private readonly IntersectionOfTwoLinkedListsSolution solution = 
+            new IntersectionOfTwoLinkedListsSolution();
+
+        [Fact]
+        public void should_null_when_both_2_lists_are_null()
+        {
+            solution.GetIntersectionNode(null, null).ShouldBeNull();
+        }
+
+        [Fact]
+        public void should_null_when_anyone_of_lists_is_null()
+        {
+            solution.GetIntersectionNode(null, new ListNode(0)).ShouldBeNull();
+        }
+
+        [Fact]
+        public void should_return_intersection_point_when_2_lists_length_are_same()
+        {
+//          A: a1
+//               ↘
+//                c1
+//               ↗            
+//          B: b1
+            var listC = new ListNode(0);
+            var listA = new ListNode(0) {next = listC};
+            var listB = new ListNode(0) {next = listC};
+
+            solution.GetIntersectionNode(listA, listB).ShouldBeSameAs(listC);
+        }
+
+        [Fact]
+        public void should_return_intersection_point_when_2_lists_length_are_not_same()
+        {
+//          A:      a1
+//                    ↘
+//                     c1
+//                    ↗            
+//          B: b1 → b2
+            var listC = new ListNode(0);
+            var listA = new ListNode(0) {next = listC};
+            var listB = new ListNode(0) {next = new ListNode(1) {next = listC}};
+
+            solution.GetIntersectionNode(listA, listB).ShouldBeSameAs(listC);
+        }
+
+        [Fact]
+        public void should_return_null_when_2_lists_length_has_no_intersection()
+        {
+//          A: a1 → a2
+//          B: b1 → b2
+            var listA = new ListNode(0) {next = new ListNode(1)};
+            var listB = new ListNode(0) {next = new ListNode(1)};
+
+            solution.GetIntersectionNode(listA, listB).ShouldBeSameAs(null);
+        }
+    }
+}
