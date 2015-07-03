a1e32c90c391337dac3ca1daf9ec3af6267f6218
r00e <young_rui@hotmail.com>
2015-07-03 18:57:18 +0800

    compare version number

---
+++ leetCode/src/165_CompareVersionNumber/CompareVersionNumbersSolution.cs
@@ -0,0 +1,22 @@
+﻿namespace leetCode._165_CompareVersionNumber
+{
+    public class CompareVersionNumbersSolution
+    {
+        public int CompareVersion(string version1, string version2)
+        {
+            var subVersions1 = version1.Split('.');
+            var subVersions2 = version2.Split('.');
+
+            for (var i = 0; i < subVersions1.Length || i < subVersions2.Length; i++)
+            {
+                var v1 = i < subVersions1.Length ? int.Parse(subVersions1[i]) : 0;
+                var v2 = i < subVersions2.Length ? int.Parse(subVersions2[i]) : 0;
+
+                if (v1 > v2) return 1;
+                if (v1 < v2) return -1;
+            }
+
+            return 0;
+        }
+    }
+}
--- leetCode/test/CompareVersionNumbersFacts.cs
+++ leetCode/test/CompareVersionNumbersFacts.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using leetCode._165_CompareVersionNumber;
+using Xunit;
 using Xunit.Should;
 
 namespace leetCode.test
@@ -59,40 +60,6 @@ namespace leetCode.test
         public void should_return_0_when_compare_1p0_and_1()
         {
             solution.CompareVersion("1.0", "1").ShouldBe(0);
-        }
-    }
-
-    internal class CompareVersionNumbersSolution
-    {
-        public int CompareVersion(string version1, string version2)
-        {
-            var v1 = version1.Split('.');
-            var v2 = version2.Split('.');
-
-            for (int i = 0, j = 0; i < v1.Length && j < v2.Length; i++, j++)
-            {
-                if (int.Parse(v1[i]) > int.Parse(v2[j])) return 1;
-                if (int.Parse(v1[i]) < int.Parse(v2[j])) return -1;
-                if (i + 1 == v1.Length && j + 1 == v2.Length) return 0;
-                if (i + 1 == v1.Length)
-                {
-                    while (++j < v2.Length)
-                    {
-                        if (int.Parse(v2[j]) != 0) return -1;
-                    }
-                    return 0;
-                }
-                if (j + 1 == v2.Length)
-                {
-                    while (++i < v1.Length)
-                    {
-                        if (int.Parse(v1[i]) != 0) return 1;
-                    }
-                    return 0;
-                }
-            }
-
-            return 0;
         }
     }
 }
