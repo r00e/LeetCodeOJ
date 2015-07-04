74c656ee55d6a42f3fb93b173aa834c3016dc32b
r00e <young_rui@hotmail.com>
2015-07-04 13:40:55 +0800

    excel sheet column title

---
+++ leetCode/src/168_ExcelSheetColumnTitleSolution/ExcelSheetColumnTitleSolution.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace leetCode._168_ExcelSheetColumnTitleSolution
+{
+    public class ExcelSheetColumnTitleSolution
+    {
+        private readonly List<string> columnTitle = new List<string>()
+        {
+            "A", "B", "C", "D", "E", "F", "G",
+            "H", "I", "J", "K", "L", "M", "N",
+            "O", "P", "Q",      "R", "S", "T",
+            "U", "V", "W",      "X", "Y", "Z"
+        };
+
+        public string ConvertToTitle(int n)
+        {
+            var result = new StringBuilder();
+
+            while (n > 0)
+            {
+                result.Insert(0, columnTitle[(n - 1)%26]);
+                n = (n - 1)/26;
+            }
+
+            return result.ToString();
+        }
+    }
+}
---
+++ leetCode/test/ExcelSheetColumnTitleFacts.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using leetCode._168_ExcelSheetColumnTitleSolution;
+using Xunit;
+using Xunit.Extensions;
+using Xunit.Should;
+
+namespace leetCode.test
+{
+    public class ExcelSheetColumnTitleFacts
+    {
+        private readonly ExcelSheetColumnTitleSolution solution = new ExcelSheetColumnTitleSolution();
+
+        [Theory]
+        [InlineData(1, "A")]
+        [InlineData(26, "Z")]
+        [InlineData(27, "AA")]
+        [InlineData(52, "AZ")]
+        [InlineData(53, "BA")]
+        [InlineData(676, "YZ")]
+        [InlineData(677, "ZA")]
+        [InlineData(702, "ZZ")]
+        [InlineData(703, "AAA")]
+        [InlineData(1048, "ANH")]
+        public void should_return_column_title_for_column(int columnNumber, string expectedColumnTitle)
+        {
+            solution.ConvertToTitle(columnNumber).ShouldBe(expectedColumnTitle);
+        }
+    }
+}
