976b96e02c8c0f038272521196ceff3adf4263cc
r00e <young_rui@hotmail.com>
2015-07-04 18:10:25 +0800

    excel sheet column number

---
+++ leetCode/src/171_ExcelSheetColumnNumberSolution/ExcelSheetColumnNumberSolution.cs
@@ -0,0 +1,15 @@
+﻿namespace leetCode._171_ExcelSheetColumnNumberSolution
+{
+    public class ExcelSheetColumnNumberSolution
+    {
+        public int TitleToNumber(string s)
+        {
+            var result = 0;
+            for (var i = 0; i < s.Length; i++)
+            {
+                result = result * 26 + (s[i] - 'A') + 1;
+            }
+            return result;
+        }
+    }
+}
---
+++ leetCode/test/ExcelSheetColumnNumberFacts.cs
@@ -0,0 +1,27 @@
+﻿using leetCode._171_ExcelSheetColumnNumberSolution;
+using Xunit.Extensions;
+using Xunit.Should;
+
+namespace leetCode.test
+{
+    public class ExcelSheetColumnNumberFacts
+    {
+        private readonly ExcelSheetColumnNumberSolution solution = new ExcelSheetColumnNumberSolution();
+
+        [Theory]
+        [InlineData("A", 1)]
+        [InlineData("Z", 26)]
+        [InlineData("AA", 27)]
+        [InlineData("AZ", 52)]
+        [InlineData("BA", 53)]
+        [InlineData("YZ", 676)]
+        [InlineData("ZA", 677)]
+        [InlineData("ZZ", 702)]
+        [InlineData("AAA", 703)]
+        [InlineData("ANH", 1048)]
+        public void should_return_column_number_for_column_title(string title, int number)
+        {
+            solution.TitleToNumber(title).ShouldBe(number);
+        }
+    }
+}
