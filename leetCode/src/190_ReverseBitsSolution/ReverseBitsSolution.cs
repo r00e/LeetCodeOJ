ae765f4011943d54044a82f6e021478a665ede7c
r00e <young_rui@hotmail.com>
2015-07-05 23:25:16 +0800

    reverse bits

---
+++ leetCode/src/190_ReverseBitsSolution/ReverseBitsSolution.cs
@@ -0,0 +1,18 @@
+﻿namespace leetCode._190_ReverseBitsSolution
+{
+    public class ReverseBitsSolution
+    {
+        public uint ReverseBits(uint n)
+        {
+            var result = (uint) 0;
+            for (var i = 0; i < 32; i++)
+            {
+                result = result << 1;
+                result = result | (n & 1);
+                n = n >> 1;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ leetCode/test/ReverseBitsFacts.cs
@@ -0,0 +1,17 @@
+﻿using leetCode._190_ReverseBitsSolution;
+using Xunit;
+using Xunit.Should;
+
+namespace leetCode.test
+{
+    public class ReverseBitsFacts
+    {
+        private readonly ReverseBitsSolution solution = new ReverseBitsSolution();
+
+        [Fact]
+        public void should_return_964176192_when_input_is_43261596()
+        {
+            solution.ReverseBits(43261596).ShouldBe((uint)964176192);
+        }
+    }
+}
