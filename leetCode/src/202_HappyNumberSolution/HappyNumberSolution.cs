6115e82393e01afbdcee2ecca8600690280f69a9
r00e <young_rui@hotmail.com>
2015-07-11 11:10:15 +0800

    happy number

---
+++ leetCode/src/202_HappyNumberSolution/HappyNumberSolution.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.ObjectModel;
+
+namespace leetCode._202_HappyNumberSolution
+{
+    public class HappyNumberSolution
+    {
+        public bool IsHappy(int n)
+        {
+            var existedSum = new Collection<int>();
+
+            while (true)
+            {
+                var sum = DigitsSquareSum(n);
+                if (sum == 1) return true;
+                if (existedSum.Contains(sum)) return false;
+                existedSum.Add(sum);
+                n = sum;
+            }
+        }
+
+        private static int DigitsSquareSum(int num)
+        {
+            var sum = 0;
+
+            while (num > 0)
+            {
+                var digit = num%10;
+                sum = sum + digit*digit;
+                num = num/10;
+            }
+            return sum;
+        }
+    }
+}
---
+++ leetCode/test/HappyNumberFacts.cs
@@ -0,0 +1,26 @@
+﻿using leetCode._202_HappyNumberSolution;
+using Xunit.Extensions;
+using Xunit.Should;
+
+namespace leetCode.test
+{
+    public class HappyNumberFacts
+    {
+        private readonly HappyNumberSolution solution = new HappyNumberSolution();
+
+        [Theory]
+        [InlineData(1, true)]
+        [InlineData(2, false)]
+        [InlineData(3, false)]
+        [InlineData(4, false)]
+        [InlineData(5, false)]
+        [InlineData(6, false)]
+        [InlineData(7, true)]
+        [InlineData(8, false)]
+        [InlineData(9, false)]
+        public void should_return_true_for_happy_number_and_fasle_for_unhappy(int num, bool expectedValue)
+        {
+            solution.IsHappy(num).ShouldBe(expectedValue);
+        }
+    }
+}
