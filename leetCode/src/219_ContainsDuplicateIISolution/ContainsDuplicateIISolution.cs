951602d6ce88620348060e06fe870df564aa09d3
r00e <young_rui@hotmail.com>
2015-07-15 23:32:26 +0800

    contains duplicate II

--- leetCode/src/219_ContainsDuplicateIISolution/ContainsDuplicateIISolution.cs
+++ leetCode/src/219_ContainsDuplicateIISolution/ContainsDuplicateIISolution.cs
@@ -4,33 +4,48 @@ namespace leetCode._219_ContainsDuplicateIISolution
 {
     public class ContainsDuplicateIISolution
     {
+//        public bool ContainsNearbyDuplicate(int[] nums, int k)
+//        {
+//            if(nums == null) return false;
+//
+//            var duplicateNumAndIndicesMapping = new Dictionary<int, IList<int>>();
+//            for (var i = 0; i < nums.Length; i++)
+//            {
+//                if (duplicateNumAndIndicesMapping.ContainsKey(nums[i]))
+//                {
+//                    duplicateNumAndIndicesMapping[nums[i]].Add(i);
+//                }
+//                else
+//                {
+//                    duplicateNumAndIndicesMapping.Add(nums[i], new List<int>() {i});
+//                }
+//            }
+//
+//            foreach (var mapping in duplicateNumAndIndicesMapping)
+//            {
+//                if (mapping.Value.Count < 2) continue;
+//                else
+//                {
+//                    for (var i = 1; i < mapping.Value.Count; i++)
+//                    {
+//                        if (mapping.Value[i] - mapping.Value[i - 1] <= k) return true;
+//                    }
+//                }
+//            }
+//
+//            return false;
+//        }
+
         public bool ContainsNearbyDuplicate(int[] nums, int k)
         {
             if(nums == null) return false;
 
-            var duplicateNumAndIndicesMapping = new Dictionary<int, IList<int>>();
+            var duplicateNums = new HashSet<int>();
+
             for (var i = 0; i < nums.Length; i++)
             {
-                if (duplicateNumAndIndicesMapping.ContainsKey(nums[i]))
-                {
-                    duplicateNumAndIndicesMapping[nums[i]].Add(i);
-                }
-                else
-                {
-                    duplicateNumAndIndicesMapping.Add(nums[i], new List<int>() {i});
-                }
-            }
-
-            foreach (var mapping in duplicateNumAndIndicesMapping)
-            {
-                if (mapping.Value.Count < 2) continue;
-                else
-                {
-                    for (var i = 1; i < mapping.Value.Count; i++)
-                    {
-                        if (mapping.Value[i] - mapping.Value[i - 1] <= k) return true;
-                    }
-                }
+                if (i > k) duplicateNums.Remove(nums[i - k - 1]);
+                if(!duplicateNums.Add(nums[i])) return true;
             }
 
             return false;
