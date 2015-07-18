f8ddba3baad570f0d062d2a9c3e9ba4cb22a0ade
r00e <young_rui@hotmail.com>
2015-07-18 23:43:48 +0800

    delete node in linked list

---
+++ leetCode/src/237_DeleteNodeInLinkedListSolution/DeleteNodeInLinkedListSolution.cs
@@ -0,0 +1,13 @@
+﻿using leetCode.Common;
+
+namespace leetCode._237_DeleteNodeInLinkedListSolution
+{
+    public class DeleteNodeInLinkedListSolution
+    {
+        public void DeleteNode(ListNode node)
+        {
+            node.val = node.next.val;
+            node.next = node.next.next;
+        }
+    }
+}
---
+++ leetCode/test/DeleteNodeInLinkedListFacts.cs
@@ -0,0 +1,29 @@
+﻿using leetCode.Common;
+using leetCode._237_DeleteNodeInLinkedListSolution;
+using Xunit;
+using Xunit.Should;
+
+namespace leetCode.test
+{
+    public class DeleteNodeInLinkedListFacts
+    {
+        private readonly DeleteNodeInLinkedListSolution solution = new DeleteNodeInLinkedListSolution();
+            
+        [Fact]
+        public void should_delete_given_node_from_linked_list()
+        {
+            var deletedNode = new ListNode(1);
+            var linkedList = new ListNode(0)
+            {
+                next = deletedNode
+            };
+            deletedNode.next = new ListNode(2);
+
+            solution.DeleteNode(deletedNode);
+
+            linkedList.val.ShouldBe(0);
+            linkedList.next.val.ShouldBe(2);
+            linkedList.next.next.ShouldBeNull();
+        } 
+    }
+}
