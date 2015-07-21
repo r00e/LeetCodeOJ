7799efb74d627755e1244492685c478b6d757c2c
r00e <young_rui@hotmail.com>
2015-07-21 09:47:36 +0800

    lowest binary common ancestor of binary search tree

---
+++ leetCode/src/235_LowestBinaryCommonAncestorOfBinarySearchTreeSolution/LowestCommonAncestorOfBinarySearchTreeSolution.cs
@@ -0,0 +1,16 @@
+﻿using leetCode.Common;
+
+namespace leetCode._235_LowestBinaryCommonAncestorOfBinarySearchTreeSolution
+{
+    public class LowestCommonAncestorOfBinarySearchTreeSolution
+    {
+        public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+            if (p.val < root.val && q.val < root.val) return LowestCommonAncestor(root.left, p, q);
+            if (p.val > root.val && q.val > root.val) return LowestCommonAncestor(root.right, p, q);
+            if (p.val == root.val) return p;
+            if (q.val == root.val) return q;
+            return root;
+        }
+    }
+}
---
+++ leetCode/test/LowestCommonAncestorOfBinarySearchTreeFacts.cs
@@ -0,0 +1,51 @@
+﻿using leetCode.Common;
+using leetCode._235_LowestBinaryCommonAncestorOfBinarySearchTreeSolution;
+using Xunit;
+using Xunit.Should;
+
+namespace leetCode.test
+{
+    public class LowestCommonAncestorOfBinarySearchTreeFacts
+    {
+        private readonly LowestCommonAncestorOfBinarySearchTreeSolution solution = new LowestCommonAncestorOfBinarySearchTreeSolution();
+            
+        [Fact]
+        public void should_return_6_for_3_and_7_in_tree637()
+        {
+            var p = new TreeNode(3);
+            var q = new TreeNode(7);
+            var tree = new TreeNode(6){left = p, right = q};
+
+            solution.LowestCommonAncestor(tree, p, q).ShouldBe(tree);
+        }
+            
+        [Fact]
+        public void should_return_2_for_2_and_4_in_tree62804()
+        {
+            var q = new TreeNode(4);
+            var p = new TreeNode(2){left = new TreeNode(0), right = q};
+            var tree = new TreeNode(6)
+            {
+                left = p, 
+                right = new TreeNode(8)
+            };
+
+            solution.LowestCommonAncestor(tree, p, q).ShouldBe(p);
+        }
+            
+        [Fact]
+        public void should_return_8_for_7_and_9_in_tree628__79()
+        {
+            var p = new TreeNode(7);
+            var q = new TreeNode(9);
+            var resultNode = new TreeNode(8) {left = p, right = q};
+            var tree = new TreeNode(6)
+            {
+                left = new TreeNode(2), 
+                right = resultNode
+            };
+
+            solution.LowestCommonAncestor(tree, p, q).ShouldBe(resultNode);
+        }
+    }
+}
