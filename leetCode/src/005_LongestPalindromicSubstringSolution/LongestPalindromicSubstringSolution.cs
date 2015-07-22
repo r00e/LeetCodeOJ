3520fd0a2619d731052f4e8a9016ed2a82c204eb
r00e <young_rui@hotmail.com>
2015-07-23 00:11:15 +0800

    longest palindromic substring

---
+++ leetCode/src/005_LongestPalindromicSubstringSolution/LongestPalindromicSubstringSolution.cs
@@ -0,0 +1,43 @@
+﻿namespace leetCode._005_LongestPalindromicSubstringSolution
+{
+    public class LongestPalindromicSubstringSolution
+    {
+        public string LongestPalindrome(string s)
+        {
+            if(s.Length <= 1) return s;
+
+            int startIndex = 0, maxLength = 0;
+            
+            for (var i = 0; i < s.Length;)
+            {
+                if (s.Length - i <= maxLength/2) break;
+                
+                var findEndIndex = i;
+                var findStartIndex = i;
+
+                while ( findEndIndex < s.Length - 1 && 
+                        s[findEndIndex] == s[findEndIndex + 1]) 
+                    findEndIndex++;
+                
+                i = findEndIndex + 1;
+                
+                while ( findEndIndex < s.Length - 1 && 
+                        findStartIndex > 0 && 
+                        s[findStartIndex - 1] == s[findEndIndex + 1])
+                {
+                    findStartIndex--;
+                    findEndIndex++;
+                }
+
+                var newPalinDromicSubstringLenght = findEndIndex - findStartIndex + 1;
+                if (newPalinDromicSubstringLenght > maxLength)
+                {
+                    maxLength = newPalinDromicSubstringLenght;
+                    startIndex = findStartIndex;
+                }
+            }
+
+            return s.Substring(startIndex, maxLength);
+        }
+    }
+}
---
+++ leetCode/test/LongestPalindromicSubstringFacts.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using leetCode._005_LongestPalindromicSubstringSolution;
+using Xunit.Extensions;
+using Xunit.Should;
+
+namespace leetCode.test
+{
+    public class LongestPalindromicSubstringFacts
+    {
+        private readonly LongestPalindromicSubstringSolution solution = new LongestPalindromicSubstringSolution();
+            
+        [Theory]
+        [InlineData("", "")]
+        [InlineData("a", "a")]
+        [InlineData("aba", "aba")]
+        [InlineData("aab", "aa")]
+        public void should_return_longest_palindrome(string s, string longestPalindromicSubstring)
+        {
+            solution.LongestPalindrome(s).ShouldBe(longestPalindromicSubstring);
+        }
+    }
+}
