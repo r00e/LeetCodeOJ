28edb79dcc25c61e75c38ace00f5df4ede02df62
r00e <young_rui@hotmail.com>
2015-08-01 13:21:55 +0800

    generate parentheses

---
+++ leetCode/src/022_GenerateParenthesesSolution/GenerateParenthesesSolution.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace leetCode._022_GenerateParenthesesSolution
+{
+    public class GenerateParenthesesSolution
+    {
+        public IList<string> GenerateParenthesis(int n)
+        {
+            var result = new List<string>();
+
+            if (n < 1) return result;
+            
+            Helper("", n, 0, 0, result);
+
+            return result;
+        }
+
+        private static IList<string> Helper(string s, int n, int left, int right, IList<string> result)
+        {
+            if (left + right == 2*n)
+            {
+                result.Add(s);
+            }
+            if (left < n)
+            {
+                Helper(s + "(", n, left+1, right, result);
+            }
+            if (right < left)
+            {
+                Helper(s + ")",n,left, right+1, result);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ leetCode/test/GenerateParenthesesFacts.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using leetCode._022_GenerateParenthesesSolution;
+using Xunit;
+using Xunit.Should;
+
+namespace leetCode.test
+{
+    public class GenerateParenthesesFacts
+    {
+        private readonly GenerateParenthesesSolution solution = new GenerateParenthesesSolution();
+
+        [Fact]
+        public void should_return_empty_when_input_0()
+        {
+            solution.GenerateParenthesis(0).ShouldBe(new List<string>());
+        }
+
+        [Fact]
+        public void should_return_expected_result_when_input_1()
+        {
+            var expectedResult = new List<string>() {"()"};
+            solution.GenerateParenthesis(1).ShouldBe(expectedResult);
+        }
+
+        [Fact]
+        public void should_return_expected_result_when_input_2()
+        {
+            var expectedResult = new List<string>() {"()()", "(())"};
+            
+            var result = solution.GenerateParenthesis(2);
+            result.Count.ShouldBe(expectedResult.Count);
+            result.ToList().ForEach(r => expectedResult.Contains(r).ShouldBeTrue());
+        }
+
+        [Fact]
+        public void should_return_expected_result_when_input_3()
+        {
+            var expectedResult = new List<string>() {"()()()", "((()))", "(()())", "()(())", "(())()"};
+            
+            var result = solution.GenerateParenthesis(3);
+            result.Count.ShouldBe(expectedResult.Count);
+            result.ToList().ForEach(r => expectedResult.Contains(r).ShouldBeTrue());
+        }
+    }
+}
