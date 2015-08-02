e4fc74a14279c22458aea5b41d2c36ab5e087c4e
r00e <young_rui@hotmail.com>
2015-08-02 23:39:02 +0800

    divide two integers

---
+++ leetCode/src/029_DivideTwoIntegersSolution/DivideTwoIntegersSolution.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace leetCode._029_DivideTwoIntegersSolution
+{
+    public class DivideTwoIntegersSolution
+    {
+        public int Divide(int dividend, int divisor)
+        {
+            if((divisor == 0) || (dividend == int.MinValue && divisor == -1)) return int.MaxValue;
+            if (divisor == 1) return dividend;
+
+            var isMinus = (dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0);
+            var count = 1;
+            var result = 0;
+            var dividendNoSign = (uint)Math.Abs((long)dividend);
+            var divisorNoSign = (uint)Math.Abs((long)divisor);
+            var tmpDivisor = divisorNoSign;
+
+            while (tmpDivisor <= dividendNoSign)
+            {
+                if (tmpDivisor << 1 > 0)
+                {
+                    tmpDivisor = tmpDivisor << 1;
+                    count = count << 1;
+
+                    if (tmpDivisor > dividendNoSign)
+                    {
+                        dividendNoSign -= (tmpDivisor >> 1);
+                        result += (count >> 1);
+                        count = 1;
+                        tmpDivisor = divisorNoSign;
+                    }
+                }
+                else
+                {
+                    dividendNoSign -= tmpDivisor;
+                    result += count;
+                    count = 1;
+                    tmpDivisor = divisorNoSign;
+                }
+            }
+
+            return isMinus ? 0 - result : result;
+        }
+    }
+}
---
+++ leetCode/test/DivideTwoIntegersFacts.cs
@@ -0,0 +1,84 @@
+﻿using leetCode._029_DivideTwoIntegersSolution;
+using Xunit;
+using Xunit.Should;
+
+namespace leetCode.test
+{
+    public class DivideTwoIntegersFacts
+    {
+        private readonly DivideTwoIntegersSolution solution = new DivideTwoIntegersSolution();
+
+        [Fact]
+        public void should_return_max_int_for_overflow()
+        {
+            solution.Divide(1, 0).ShouldBe(int.MaxValue);
+            solution.Divide(int.MinValue, -1).ShouldBe(int.MaxValue);
+        }
+
+        [Fact]
+        public void should_return_3_for_3_1()
+        {
+            solution.Divide(3, 1).ShouldBe(3);
+        }
+
+        [Fact]
+        public void should_return_5_for_5_1()
+        {
+            solution.Divide(5, 1).ShouldBe(5);
+        }
+
+        [Fact]
+        public void should_return_0_for_0_1()
+        {
+            solution.Divide(0, 1).ShouldBe(0);
+        }
+
+        [Fact]
+        public void should_return_0_for_0_3()
+        {
+            solution.Divide(0, 3).ShouldBe(0);
+        }
+
+        [Fact]
+        public void should_return_1_for_3_2()
+        {
+            solution.Divide(3, 2).ShouldBe(1);
+        }
+
+        [Fact]
+        public void should_return_2_for_4_2()
+        {
+            solution.Divide(4, 2).ShouldBe(2);
+        }
+
+        [Fact]
+        public void should_return_3_for_11_3()
+        {
+            solution.Divide(11, 3).ShouldBe(3);
+        }
+
+        [Fact]
+        public void should_return_m1_for_1_m1()
+        {
+            solution.Divide(1, -1).ShouldBe(-1);
+        }
+
+        [Fact]
+        public void should_return_m2_for_5_m2()
+        {
+            solution.Divide(5, -2).ShouldBe(-2);
+        }
+
+        [Fact]
+        public void should_return_1073741823_for_int_max_2()
+        {
+            solution.Divide(int.MaxValue, 2).ShouldBe(int.MaxValue/2);
+        }
+
+        [Fact]
+        public void should_return_m1073741824_for_int_min_2()
+        {
+            solution.Divide(int.MinValue, 2).ShouldBe(int.MinValue/2);
+        }
+    }
+}
