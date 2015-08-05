c9ffab415663ffb5bdce336e96d612539245172f
r00e <young_rui@hotmail.com>
2015-08-05 08:05:29 +0800

    nex permutation

---
+++ leetCode/src/031_NextPermutationSolution/NextPermutationSolution.cs
@@ -0,0 +1,77 @@
+﻿namespace leetCode._031_NextPermutationSolution
+{
+    public class NextPermutationSolution
+    {
+        public void NextPermutation(int[] nums)
+        {
+            if (nums.Length < 2) return;
+
+            var latter = nums.Length - 1;
+            var former = nums.Length - 2;
+
+            for (; former >= 0; latter--, former--)
+            {
+                if (nums[latter] > nums[former] && latter == nums.Length - 1 && former == nums.Length - 2)
+                {
+                    Swap(ref nums[latter], ref nums[former]);
+                    break;
+                }
+
+                if (nums[latter] > nums[former])
+                {
+                    latter = nums.Length - 1;
+                    break;
+                }
+            }
+
+            if (former < 0)
+            {
+                Sort(nums, 0, nums.Length - 1);
+            }
+
+            while (former >=0 && nums[latter] <= nums[former] && latter > former)
+            {
+                latter--;
+            }
+
+            if (former >=0 && latter != former)
+            {
+                Swap(ref nums[latter], ref nums[former]);
+                Sort(nums, former + 1, nums.Length - 1);
+            }
+        }
+
+        private void Sort(int[] nums, int startIndex, int endIndex)
+        {
+            if (startIndex > endIndex) return;
+
+            var index = SortPart(nums, startIndex, endIndex);
+            Sort(nums, startIndex, index - 1);
+            Sort(nums, index + 1, endIndex);
+        }
+
+        private int SortPart(int[] nums, int startIndex, int endIndex)
+        {
+            var key = nums[startIndex];
+                
+            while (startIndex < endIndex)
+            {
+                while (nums[endIndex] >= key && startIndex < endIndex) endIndex--;
+                nums[startIndex] = nums[endIndex];
+                while (nums[startIndex] <= key && startIndex < endIndex) startIndex++;
+                nums[endIndex] = nums[startIndex];
+            }
+
+            nums[startIndex] = key;
+
+            return startIndex;
+        }
+
+        private static void Swap(ref int latter, ref int former)
+        {
+            var tmp = latter;
+            latter = former;
+            former = tmp;
+        }
+    }
+}
---
+++ leetCode/test/NextPermutationFacts.cs
@@ -0,0 +1,83 @@
+﻿using leetCode._031_NextPermutationSolution;
+using Xunit;
+using Xunit.Should;
+
+namespace leetCode.test
+{
+    public class NextPermutationFacts
+    {
+        private readonly NextPermutationSolution solution = new NextPermutationSolution();
+
+        [Fact]
+        public void should_get_empty_for_empty()
+        {
+            var nums = new int[] {};
+
+            solution.NextPermutation(nums);
+            nums.ShouldBe(nums);
+        }
+
+        [Fact]
+        public void should_get_1_for_1()
+        {
+            var nums = new int[] {1};
+
+            solution.NextPermutation(nums);
+            nums.ShouldBe(new int[]{1});
+        }
+
+        [Fact]
+        public void should_get_12_for_12()
+        {
+            var nums = new int[] {1, 2};
+
+            solution.NextPermutation(nums);
+            nums.ShouldBe(new int[]{2, 1});
+        }
+
+        [Fact]
+        public void should_get_132_for_123()
+        {
+            var nums = new int[] {1, 2, 3};
+
+            solution.NextPermutation(nums);
+            nums.ShouldBe(new int[]{1, 3, 2});
+        }
+
+        [Fact]
+        public void should_get_213_for_132()
+        {
+            var nums = new int[] {1, 3, 2};
+
+            solution.NextPermutation(nums);
+            nums.ShouldBe(new int[]{2, 1, 3});
+        }
+
+        [Fact]
+        public void should_get_231_for_213()
+        {
+            var nums = new int[] {2, 1, 3};
+
+            solution.NextPermutation(nums);
+            nums.ShouldBe(new int[]{2, 3, 1});
+        }
+
+        [Fact]
+        public void should_get_123_for_321()
+        {
+            var nums = new int[] {3, 2, 1};
+
+            solution.NextPermutation(nums);
+            nums.ShouldBe(new int[]{1, 2, 3});
+        }
+
+        [Fact]
+        public void should_get_552347_for_547532()
+        {
+            var nums = new int[] {5, 4, 7, 5, 3, 2};
+
+            solution.NextPermutation(nums);
+            nums.ShouldBe(new int[]{5, 5, 2, 3, 4, 7});
+        }
+    }
+}
