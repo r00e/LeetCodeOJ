629779ec07f9ef07f9d2826a4457b517251c0f28
r00e <young_rui@hotmail.com>
2015-08-08 22:05:23 +0800

    combination sum

---
+++ leetCode/src/039_CombinationSumSolution/CombinationSumSolution.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace leetCode._039_CombinationSumSolution
+{
+    public class CombinationSumSolution
+    {
+        public IList<IList<int>> CombinationSum(int[] candidates, int target)
+        {
+            var result = new List<IList<int>>();
+
+            for (var i = candidates.Length - 1; i >= 0; i--)
+            {
+                if(i < candidates.Length - 1 && candidates[i] == candidates[i+1]) continue;
+                if(candidates[i] > target) continue;
+                if(candidates[i] == target) result.Add(new List<int> {candidates[i]});
+                if(candidates[i] < target)
+                {
+                    var solution = CombinationSum(candidates.Where(c => c <= candidates[i]).ToArray(), target - candidates[i]).ToList();
+                    solution.ForEach(s => s.Add(candidates[i]));
+                    solution.ForEach(result.Add);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ leetCode/test/CombinationSumFacts.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections.Generic;
+using leetCode._039_CombinationSumSolution;
+using Xunit;
+using Xunit.Should;
+
+namespace leetCode.test
+{
+    public class CombinationSumFacts
+    {
+        private readonly CombinationSumSolution solution = new CombinationSumSolution();
+
+        [Fact]
+        public void should_return_empty_when_target_is_0_and_candidates_are_empty()
+        {
+            solution.CombinationSum(new int[] {}, 0).ShouldBeEmpty();
+        }
+
+        [Fact]
+        public void should_return_1_when_target_is_1_and_candidates_are_1()
+        {
+            var candidates = new int[] {1};
+            const int target = 1;
+            var expected = new List<IList<int>> {new List<int>() {1}};
+            solution.CombinationSum(candidates, target).ShouldBe(expected);
+        }
+
+        [Fact]
+        public void should_return_empty_when_target_is_0_and_candidates_are_1()
+        {
+            var candidates = new int[] {1};
+            const int target = 0;
+            var expected = new List<IList<int>>();
+            solution.CombinationSum(candidates, target).ShouldBe(expected);
+        }
+
+        [Fact]
+        public void should_return_12_when_target_is_3_and_candidates_are_12()
+        {
+            var candidates = new int[] {1, 2};
+            const int target = 3;
+            var expected = new List<IList<int>> {new List<int> {1, 2}, new List<int> {1, 1, 1}};
+            solution.CombinationSum(candidates, target).ShouldBe(expected);
+        }
+
+        [Fact]
+        public void should_return_2_11_when_target_is_2_and_candidates_are_12()
+        {
+            var candidates = new int[] {1, 2};
+            const int target = 2;
+            var expected = new List<IList<int>> {new List<int>() {2}, new List<int>() {1, 1}};
+            solution.CombinationSum(candidates, target).ShouldBe(expected);
+        }
+
+        [Fact]
+        public void should_return_empty_when_target_is_5_and_candidates_are_24()
+        {
+            var candidates = new int[] {2, 4};
+            const int target = 5;
+            var expected = new List<IList<int>>();
+            solution.CombinationSum(candidates, target).ShouldBe(expected);
+        }
+
+        [Fact]
+        public void should_return_5_when_target_is_5_and_candidates_are_2455()
+        {
+            var candidates = new int[] {2, 4, 5, 5};
+            const int target = 5;
+            var expected = new List<IList<int>>{new List<int>{5}};
+            solution.CombinationSum(candidates, target).ShouldBe(expected);
+        }
+
+        [Fact]
+        public void should_return_42_222_when_target_is_6_and_candidates_are_224()
+        {
+            var candidates = new int[] {2, 2, 4};
+            const int target = 6;
+            var expected = new List<IList<int>> {new List<int> {2, 4}, new List<int> {2, 2, 2}};
+            solution.CombinationSum(candidates, target).ShouldBe(expected);
+        }
+    }
+}
