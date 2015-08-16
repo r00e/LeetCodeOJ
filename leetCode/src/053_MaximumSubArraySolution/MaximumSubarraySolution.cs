1804dd626c407682adc37963cad56ae86e77e620
r00e <young_rui@hotmail.com>
2015-08-16 12:16:13 +0800

    maximum subarray solution

---
+++ leetCode/src/053_MaximumSubArraySolution/MaximumSubarraySolution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace leetCode._053_MaximumSubArraySolution
+{
+    public class MaximumSubarraySolution
+    {
+        public int MaxSubArray(int[] nums)
+        {
+            if(nums.Length == 0) return 0;
+            
+            var maxEndingHere = nums[0];
+            var max = nums[0];
+
+            for (var i = 1; i < nums.Length; i++)
+            {
+                maxEndingHere = Math.Max(maxEndingHere + nums[i], nums[i]);
+                max = Math.Max(maxEndingHere, max);
+            }
+            return max;
+        }
+    }
+}
---
+++ leetCode/test/MaximumSubarrayFacts.cs
@@ -0,0 +1,35 @@
+﻿using leetCode._053_MaximumSubArraySolution;
+using Xunit;
+using Xunit.Should;
+
+namespace leetCode.test
+{
+    public class MaximumSubarrayFacts
+    {
+        private readonly MaximumSubarraySolution solution = new MaximumSubarraySolution();
+
+        [Fact]
+        public void should_return_0_for_empty()
+        {
+            solution.MaxSubArray(new int[]{}).ShouldBe(0);
+        }
+
+        [Fact]
+        public void should_return_1_for_1()
+        {
+            solution.MaxSubArray(new int[]{1}).ShouldBe(1);
+        }
+
+        [Fact]
+        public void should_return_3_for_12()
+        {
+            solution.MaxSubArray(new int[] {1, 2}).ShouldBe(3);
+        }
+
+        [Fact]
+        public void should_return_5_for_m123()
+        {
+            solution.MaxSubArray(new int[] {-1, 2, 3}).ShouldBe(5);
+        }
+    }
+}
