c57ff85afa11f68efb15cfd1e467039428f02d24
r00e <young_rui@hotmail.com>
2015-08-19 08:48:43 +0800

    jump game solution

---
+++ leetCode/src/055_JumpGameSolution/JumpGameSolution.cs
@@ -0,0 +1,18 @@
+﻿namespace leetCode._055_JumpGameSolution
+{
+    public class JumpGameSolution
+    {
+        public bool CanJump(int[] nums)
+        {
+            if (nums.Length == 0) return false;
+
+            var last = nums.Length - 1;
+            for (var i = nums.Length - 2; i >= 0; i--)
+            {
+                if (i + nums[i] >= last) last = i;
+            }
+
+            return last <= 0;
+        }
+    }
+}
---
+++ leetCode/test/JumpGameFacts.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using leetCode._055_JumpGameSolution;
+using Xunit;
+using Xunit.Extensions;
+using Xunit.Should;
+
+namespace leetCode.test
+{
+    public class JumpGameFacts
+    {
+        private readonly JumpGameSolution solution = new JumpGameSolution();
+
+        [Theory]
+        [InlineData(new int[] { }, false)]
+        [InlineData(new int[] {0}, true)]
+        [InlineData(new int[] {1}, true)]
+        [InlineData(new int[] {1, 0}, true)]
+        [InlineData(new int[] {0, 0}, false)]
+        [InlineData(new int[] {1, 0, 0}, false)]
+        [InlineData(new int[] {1, 1, 0}, true)]
+        [InlineData(new int[] {2, 2, 0, 0}, true)]
+        [InlineData(new int[] {3, 2, 1, 0, 4}, false)]
+        public void should_return_expectedValue_for_givenArray(int[] givenArray, bool expectedValue)
+        {
+            solution.CanJump(givenArray).ShouldBe(expectedValue);
+        }
+    }
+}
