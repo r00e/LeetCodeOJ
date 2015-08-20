662b60bee1209db0ad2da4ccc62ea10c4dbfae6b
r00e <young_rui@hotmail.com>
2015-08-20 23:49:57 +0800

    spiral matrix II solution

---
+++ leetCode/src/059_SpiralMatrixIISolution/SprialMatrixIISolution.cs
@@ -0,0 +1,30 @@
+﻿namespace leetCode._059_SpiralMatrixIISolution
+{
+    public class SprialMatrixIISolution
+    {
+        public int[,] GenerateMatrix(int n)
+        {
+            if (n < 1) return new int[,]{};
+
+            var result = new int[n,n];
+
+            int left = -1, top = 0, right = n - 1, bottom = n - 1, num = 1;
+
+            while (num <= n*n)
+            {
+                // from left to right
+                for (var i = ++left; i <= right; i++) result[top, i] = num++;
+                if(num > n*n) break;
+                // from top to bottom
+                for (var i = ++top; i <= bottom; i++) result[i, right] = num++;
+                if (num > n*n) break;
+                // from right to left
+                for (var i = --right; i >= left; i--) result[bottom, i] = num++;
+                // from bottom to top
+                for (var i = --bottom; i >= top; i--) result[i, left] = num++;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ leetCode/test/SpiralMatrixIIFacts.cs
@@ -0,0 +1,44 @@
+﻿using leetCode._059_SpiralMatrixIISolution;
+using Xunit;
+using Xunit.Should;
+
+namespace leetCode.test
+{
+    public class SpiralMatrixIIFacts
+    {
+        private readonly SprialMatrixIISolution solution = new SprialMatrixIISolution();
+
+        [Fact]
+        public void should_return_null_for_any_n_less_than_1()
+        {
+            solution.GenerateMatrix(0).ShouldBeNull();
+        }
+
+        [Fact]
+        public void should_return_matrix_1_for_1()
+        {
+            solution.GenerateMatrix(1).ShouldBe(new int[,]{{1}});
+        }
+
+        [Fact]
+        public void should_return_matrix_1243_for_2()
+        {
+            solution.GenerateMatrix(2).ShouldBe(new int[,]
+            {
+                {1, 2},
+                {4, 3}
+            });
+        }
+
+        [Fact]
+        public void should_return_matrix_123_for_3()
+        {
+            solution.GenerateMatrix(3).ShouldBe(new int[,]
+            {
+                {1, 2, 3},
+                {8, 9, 4},
+                {7, 6, 5}
+            });
+        }
+    }
+}
