1eb54417f6a9bb1aff7ae03d37a849f14146de2f
r00e <young_rui@hotmail.com>
2015-08-23 16:44:19 +0800

    set matrix zeroes solution

---
+++ leetCode/src/073_SetMatrixZeroesSolution/SetMatrixZeroesSolution.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace leetCode._073_SetMatrixZeroesSolution
+{
+    public class SetMatrixZeroesSolution
+    {
+        public void SetZeroes(int[,] matrix)
+        {
+            var toBeZeroRows = new HashSet<int>();
+            var toBeZeroColumns = new HashSet<int>();
+
+            for (var i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (var j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] == 0)
+                    {
+                        toBeZeroRows.Add(i);
+                        toBeZeroColumns.Add(j);
+                    }
+                }
+            }
+
+            toBeZeroRows.ToList().ForEach(row =>
+            {
+                for (var i = 0; i < matrix.GetLength(1); i++)
+                {
+                    matrix[row, i] = 0;
+                }
+            });
+
+            toBeZeroColumns.ToList().ForEach(column =>
+            {
+                for (var i = 0; i < matrix.GetLength(0); i++)
+                {
+                    matrix[i, column] = 0;
+                }
+            });
+        }
+    }
+}
---
+++ leetCode/test/SetMatrixZeroesFacts.cs
@@ -0,0 +1,85 @@
+﻿using leetCode._073_SetMatrixZeroesSolution;
+using Xunit;
+using Xunit.Should;
+
+namespace leetCode.test
+{
+    public class SetMatrixZeroesFacts
+    {
+        private readonly SetMatrixZeroesSolution solution = new SetMatrixZeroesSolution();
+
+        [Fact]
+        public void should_do_nothing_to_empty_matrix()
+        {
+            var matrix = new int[0, 0];
+            solution.SetZeroes(matrix);
+            matrix.ShouldBe(new int[0,0]);
+        }
+
+        [Fact]
+        public void should_update_1x1_matrix_1_to_1()
+        {
+            var matrix = new int[1, 1]{{1}};
+            solution.SetZeroes(matrix);
+            matrix.ShouldBe(new int[1,1]{{1}});
+        }
+
+        [Fact]
+        public void should_update_1x1_matrix_0_to_0()
+        {
+            var matrix = new int[1, 1]{{0}};
+            solution.SetZeroes(matrix);
+            matrix.ShouldBe(new int[1,1]{{0}});
+        }
+
+        [Fact]
+        public void should_update_1x2_matrix_10_to_00()
+        {
+            var matrix = new int[1, 2] {{1, 0}};
+            solution.SetZeroes(matrix);
+            matrix.ShouldBe(new int[1, 2] {{0, 0}});
+        }
+
+        [Fact]
+        public void should_update_2x1_matrix_10_to_00()
+        {
+            var matrix = new int[2, 1] {{1}, {0}};
+            solution.SetZeroes(matrix);
+            matrix.ShouldBe(new int[2, 1] {{0}, {0}});
+        }
+
+        [Fact]
+        public void should_update_2x2_matrix_1101_to_0100()
+        {
+            var matrix = new int[2, 2]
+            {
+                {1,1}, 
+                {0,1}
+            };
+            solution.SetZeroes(matrix);
+            matrix.ShouldBe(new int[2, 2]
+            {
+                {0, 1}, 
+                {0, 0}
+            });
+        }
+
+        [Fact]
+        public void should_update_3x3_matrix_111101111_to_101000101()
+        {
+            var matrix = new int[3, 3]
+            {
+                {1,1,1}, 
+                {1,0,1},
+                {1,1,1},
+            };
+            solution.SetZeroes(matrix);
+            matrix.ShouldBe(new int[3, 3]
+            {
+                {1, 0, 1},
+                {0, 0, 0},
+                {1, 0, 1}
+            });
+        }
+    }
+}
