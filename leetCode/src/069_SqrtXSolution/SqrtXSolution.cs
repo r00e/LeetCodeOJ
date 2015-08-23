6f93333b8a84d5fc21ebd2f6bac3ed8b6a9546a6
r00e <young_rui@hotmail.com>
2015-08-23 12:26:21 +0800

    sqrt x solution

---
+++ leetCode/src/069_SqrtXSolution/SqrtXSolution.cs
@@ -0,0 +1,39 @@
+﻿namespace leetCode._069_SqrtXSolution
+{
+    public class SqrtXSolution
+    {
+        // Actually this is NOT a well structed Square Root question.
+        // Because the return value is an integer number, we can just return the approximation for given number
+        // For example:
+        // 1 for 1
+        // 1 for 2
+        // 1 for 3
+        // 2 for 4
+        // 2 for 5
+
+        public int MySqrt(int x)
+        {
+            if (x <= 0) return 0;
+
+            var left = 1;
+            var right = x;
+            var mid = (left + right)/2;
+
+            while (left <= right)
+            {
+                if (mid > x/mid)
+                {
+                    right = mid - 1;
+                    mid = (left + right) / 2;
+                }
+                else
+                {
+                    left = mid + 1;
+                    mid = (left + right) / 2;
+                }                
+            }
+
+            return mid;
+        }
+    }
+}
--- leetCode/test/SqrtXFacts.cs
+++ leetCode/test/SqrtXFacts.cs
@@ -1,4 +1,5 @@
-﻿using Xunit;
+﻿using leetCode._069_SqrtXSolution;
+using Xunit;
 using Xunit.Extensions;
 using Xunit.Should;
 
@@ -13,6 +14,7 @@ namespace leetCode.test
         [InlineData(0, 0)]
         [InlineData(1, 1)]
         [InlineData(2, 1)]
+        [InlineData(3, 1)]
         [InlineData(4, 2)]
         [InlineData(6, 2)]
         [InlineData(8, 2)]
@@ -22,43 +24,6 @@ namespace leetCode.test
         public void should_return_square_root_for_given_number(int givenNumber, int squareRoot)
         {
             solution.MySqrt(givenNumber).ShouldBe(squareRoot);
-        }
-    }
-
-    internal class SqrtXSolution
-    {
-        // Actually this is NOT a well structed Square Root question.
-        // Because the return value is an integer number, we can just return the approximation for given number
-        // For example:
-        // 1 for 1
-        // 1 for 2
-        // 1 for 3
-        // 2 for 4
-        // 2 for 5
-
-        public int MySqrt(int x)
-        {
-            if (x <= 0) return 0;
-
-            var left = 1;
-            var right = x;
-            var mid = (left + right)/2;
-
-            while (left != mid && right != mid)
-            {
-                if (mid * mid > x)
-                {
-                    right = mid;
-                    mid = (left + right) / 2;
-                }
-                else
-                {
-                    left = mid;
-                    mid = (left + right) / 2;
-                }                
-            }
-
-            return mid;
         }
     }
 }
