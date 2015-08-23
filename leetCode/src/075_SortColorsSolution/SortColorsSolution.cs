3e84a7f0dd21498687f3bf3df35cf4765306e1e1
r00e <young_rui@hotmail.com>
2015-08-23 22:15:16 +0800

    sort colors solution

---
+++ leetCode/src/075_SortColorsSolution/SortColorsSolution.cs
@@ -0,0 +1,19 @@
+﻿using leetCode.Common;
+
+namespace leetCode._075_SortColorsSolution
+{
+    public class SortColorsSolution
+    {
+        public void SortColors(int[] nums)
+        {
+            const int red = 0, blue = 2;
+            int head = 0, tail = nums.Length - 1;
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                while(nums[i] == blue && i < tail) CommonMethod.Swap(ref nums[i], ref nums[tail--]);
+                while(nums[i] == red && head < i) CommonMethod.Swap(ref nums[i], ref nums[head++]);
+            }
+        }
+    }
+}
---
+++ leetCode/test/SortColorsFacts.cs
@@ -0,0 +1,43 @@
+﻿using leetCode._075_SortColorsSolution;
+using Xunit;
+using Xunit.Should;
+
+namespace leetCode.test
+{
+    public class SortColorsFacts
+    {
+        private readonly SortColorsSolution solution = new SortColorsSolution();
+
+        [Fact]
+        public void should_do_nothing_for_empty()
+        {
+            var nums = new int[]{};
+            solution.SortColors(nums);
+            nums.ShouldBe(new int[]{});
+        }
+
+        [Fact]
+        public void should_sort_color_for_102()
+        {
+            var nums = new int[] {1, 0, 2};
+            solution.SortColors(nums);
+            nums.ShouldBe(new int[] {0, 1, 2});
+        }
+
+        [Fact]
+        public void should_sort_color_for_012()
+        {
+            var nums = new int[] {0, 1, 2};
+            solution.SortColors(nums);
+            nums.ShouldBe(new int[] {0, 1, 2});
+        }
+
+        [Fact]
+        public void should_sort_color_for_0120()
+        {
+            var nums = new int[] {0, 1, 2, 0};
+            solution.SortColors(nums);
+            nums.ShouldBe(new int[] {0, 0, 1, 2});
+        }
+    }
+}
