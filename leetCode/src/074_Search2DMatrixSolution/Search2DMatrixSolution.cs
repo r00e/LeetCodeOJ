d8fddba3a1497eecb25fbd6135cafa40010136a4
r00e <young_rui@hotmail.com>
2015-08-23 18:59:12 +0800

    search 2D matrix solution

---
+++ leetCode/src/074_Search2DMatrixSolution/Search2DMatrixSolution.cs
@@ -0,0 +1,26 @@
+﻿namespace leetCode._074_Search2DMatrixSolution
+{
+    public class Search2DMatrixSolution
+    {
+        public bool SearchMatrix(int[,] matrix, int target)
+        {
+            if(matrix.Length < 1) return false;
+
+            int m = matrix.GetLength(0), n = matrix.GetLength(1);
+            int topLeft = 0, bottomRight = matrix.Length - 1;
+
+            while (topLeft <= bottomRight)
+            {
+                var mid = (topLeft + bottomRight) / 2;
+                var x = mid/n;
+                var y = mid%n;
+
+                if (target == matrix[x, y]) return true;
+                if (target < matrix[x, y]) bottomRight = mid - 1;
+                else topLeft = mid + 1;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ leetCode/test/Search2DMatrixFacts.cs
@@ -0,0 +1,46 @@
+﻿using leetCode._074_Search2DMatrixSolution;
+using Xunit;
+using Xunit.Should;
+
+namespace leetCode.test
+{
+    public class Search2DMatrixFacts
+    {
+        private readonly Search2DMatrixSolution solution = new Search2DMatrixSolution();
+
+        [Fact]
+        public void should_false_for_empty_matrix()
+        {
+            solution.SearchMatrix(new int[0, 0], 0).ShouldBeFalse();
+        }
+
+        [Fact]
+        public void should_false_when_search_0_in_1x1_matrix_1()
+        {
+            solution.SearchMatrix(new int[,]{{1}}, 0).ShouldBeFalse();
+        }
+
+        [Fact]
+        public void should_true_when_search_1_in_1x1_matrix_1()
+        {
+            solution.SearchMatrix(new int[,]{{1}}, 1).ShouldBeTrue();
+        }
+
+        [Fact]
+        public void should_true_when_search_2_in_1x2_matrix_12()
+        {
+            solution.SearchMatrix(new int[,] {{1, 2}}, 2).ShouldBeTrue();
+        }
+
+        [Fact]
+        public void should_true_when_search_4_in_3x3_matrix_123456()
+        {
+            solution.SearchMatrix(new int[,]
+            {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+            }, 4).ShouldBeTrue();
+        }
+    }
+}
