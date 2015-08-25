da60333eba6635f243b2069f3cf11efb757d27bb
r00e <young_rui@hotmail.com>
2015-08-25 08:22:36 +0800

    combination

---
+++ leetCode/src/077_CombinationsSolution/CombinationsSolution.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace leetCode._077_CombinationsSolution
+{
+    public class CombinationsSolution
+    {
+        public IList<IList<int>> Combine(int n, int k)
+        {
+            if (k == 0 || k > n) return new List<IList<int>>();
+
+            var combineResults = new List<IList<int>>();
+            var combineResult = new List<int>();
+
+            Combine(combineResults, combineResult, n, k, 1, 0);
+
+            return combineResults;
+        }
+
+        private void Combine(List<IList<int>> combineResults, List<int> combineResult, int totalNum, int wantGet, int start, int count)
+        {
+            if (count == wantGet)
+            {
+                var tmp = new List<int>();
+                combineResult.ForEach(tmp.Add);
+                combineResults.Add(tmp);
+                return;
+            }
+            for (var i = start; i <= totalNum; i++)
+            {
+                combineResult.Add(i);
+                Combine(combineResults, combineResult, totalNum, wantGet, i + 1, count + 1);
+                combineResult.Remove(i);
+            }
+        }
+    }
+}
--- leetCode/test/CombinationsFacts.cs
+++ leetCode/test/CombinationsFacts.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using leetCode._077_CombinationsSolution;
 using Xunit;
 using Xunit.Should;
 
@@ -49,38 +50,6 @@ namespace leetCode.test
             {
                 new List<int>{1, 2}
             });
-        }
-    }
-
-    internal class CombinationsSolution
-    {
-        public IList<IList<int>> Combine(int n, int k)
-        {
-            if (k == 0 || k > n) return new List<IList<int>>();
-
-            var combineResults = new List<IList<int>>();
-            var combineResult = new List<int>();
-
-            Combine(combineResults, combineResult, n, k, 1, 0);
-
-            return combineResults;
-        }
-
-        private void Combine(List<IList<int>> combineResults, List<int> combineResult, int totalNum, int wantGet, int start, int count)
-        {
-            if (count == wantGet)
-            {
-                var tmp = new List<int>();
-                combineResult.ForEach(tmp.Add);
-                combineResults.Add(tmp);
-                return;
-            }
-            for (var i = start; i <= totalNum; i++)
-            {
-                combineResult.Add(i);
-                Combine(combineResults, combineResult, totalNum, wantGet, i + 1, count + 1);
-                combineResult.Remove(i);
-            }
         }
     }
 }
