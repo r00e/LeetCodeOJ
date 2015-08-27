72cd1adb3606e2af67763f6767840ca5b95a9cd3
r00e <young_rui@hotmail.com>
2015-08-28 07:27:31 +0800

    remove duplicates from sorted array II

---
+++ leetCode/src/080_RemoveDuplicatesFromSortedArrayIISolution/RemoveDuplicatesFromSortedArrayIISolution.cs
@@ -0,0 +1,23 @@
+﻿namespace leetCode._080_RemoveDuplicatesFromSortedArrayIISolution
+{
+    public class RemoveDuplicatesFromSortedArrayIISolution
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length < 3) return nums.Length;
+
+            int length = 2, latter = 2;
+
+            while (latter < nums.Length)
+            {
+                if(nums[length - 2] != nums[latter])
+                {
+                    nums[length++] = nums[latter];
+                }
+                latter++;
+            }
+
+            return length;
+        }
+    }
+}
---
+++ leetCode/test/RemoveDuplicatesFromSortedArrayIIFacts.cs
@@ -0,0 +1,53 @@
+﻿using leetCode._080_RemoveDuplicatesFromSortedArrayIISolution;
+using Xunit;
+using Xunit.Should;
+
+namespace leetCode.test
+{
+    public class RemoveDuplicatesFromSortedArrayIIFacts
+    {
+        private readonly RemoveDuplicatesFromSortedArrayIISolution solution = new RemoveDuplicatesFromSortedArrayIISolution();
+
+        [Fact]
+        public void should_return_0_for_empty()
+        {
+            solution.RemoveDuplicates(new int[]{}).ShouldBe(0);
+        }
+
+        [Fact]
+        public void should_return_1_for_0()
+        {
+            solution.RemoveDuplicates(new int[]{0}).ShouldBe(1);
+        }
+
+        [Fact]
+        public void should_return_2_for_01()
+        {
+            solution.RemoveDuplicates(new int[] {0, 1}).ShouldBe(2);
+        }
+
+        [Fact]
+        public void should_return_3_for_112()
+        {
+            solution.RemoveDuplicates(new int[] {1, 1, 2}).ShouldBe(3);
+        }
+
+        [Fact]
+        public void should_return_3_for_0111()
+        {
+            solution.RemoveDuplicates(new int[] {0, 1, 1, 1}).ShouldBe(3);
+        }
+
+        [Fact]
+        public void should_return_3_for_012()
+        {
+            solution.RemoveDuplicates(new int[] {0, 1, 2}).ShouldBe(3);
+        }
+
+        [Fact]
+        public void should_return_3_for_1112()
+        {
+            solution.RemoveDuplicates(new int[] {1, 1, 1, 2}).ShouldBe(3);
+        }
+    }
+}
