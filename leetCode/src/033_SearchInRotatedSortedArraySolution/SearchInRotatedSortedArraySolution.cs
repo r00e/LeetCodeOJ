87bc210d56576670f0595935bec814c6b6e3fb38
r00e <young_rui@hotmail.com>
2015-08-29 10:49:14 +0800

    search in rotated sorted array solution

---
+++ leetCode/src/033_SearchInRotatedSortedArraySolution/SearchInRotatedSortedArraySolution.cs
@@ -0,0 +1,30 @@
+﻿namespace leetCode._033_SearchInRotatedSortedArraySolution
+{
+    public class SearchInRotatedSortedArraySolution
+    {
+        public int Search(int[] nums, int target)
+        {
+            int realStart = 0, end = nums.Length - 1, start = 0;
+
+            while (realStart < end)
+            {
+                var mid = (realStart + end)/2;
+                if (nums[mid] > nums[end]) realStart = mid + 1;
+                else end = mid;
+            }
+
+            end = nums.Length - 1;
+
+            while (start <= end)
+            {
+                var mid = (start + end)/2;
+                var realMid = (realStart + mid)%nums.Length;
+                if (target == nums[realMid]) return realMid;
+                if (target > nums[realMid]) start = mid + 1;
+                else end = mid - 1;
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ leetCode/test/SearchInRotatedSortedArrayFacts.cs
@@ -0,0 +1,66 @@
+﻿using System.Linq;
+using leetCode._033_SearchInRotatedSortedArraySolution;
+using Xunit;
+using Xunit.Should;
+
+namespace leetCode.test
+{
+    public class SearchInRotatedSortedArrayFacts
+    {
+        private readonly SearchInRotatedSortedArraySolution solution = new SearchInRotatedSortedArraySolution();
+
+        [Fact]
+        public void should_return_m1_for_empty()
+        {
+            solution.Search(new int[] {}, 0).ShouldBe(-1);
+        }
+
+        [Fact]
+        public void should_return_0_when_search_0_in_0()
+        {
+            solution.Search(new[] {0}, 0).ShouldBe(0);
+        }
+
+        [Fact]
+        public void should_return_1_when_search_1_in_01()
+        {
+            solution.Search(new[] {0, 1}, 1).ShouldBe(1);
+        }
+
+        [Fact]
+        public void should_return_1_when_search_1_in_012()
+        {
+            solution.Search(new[] {0, 1, 2}, 1).ShouldBe(1);
+        }
+
+        [Fact]
+        public void should_return_2_when_search_1_in_3012()
+        {
+            solution.Search(new[] {3, 0, 1, 2}, 1).ShouldBe(2);
+        }
+
+        [Fact]
+        public void should_return_m1_when_search_4_in_3012()
+        {
+            solution.Search(new[] {3, 0, 1, 2}, 4).ShouldBe(-1);
+        }
+
+        [Fact]
+        public void should_return_m1_when_search_2_in_1()
+        {
+            solution.Search(new[] {1}, 2).ShouldBe(-1);
+        }
+
+        [Fact]
+        public void should_return_m1_when_search_0_in_31()
+        {
+            solution.Search(new[] {3, 1}, 0).ShouldBe(-1);
+        }
+
+        [Fact]
+        public void should_return_m1_when_search_0_in_13()
+        {
+            solution.Search(new[] {1, 3}, 0).ShouldBe(-1);
+        }
+    }
+}
