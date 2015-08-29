fbbaa574f63a613d33160b73b4efcf788a671d65
r00e <young_rui@hotmail.com>
2015-08-29 13:28:49 +0800

    search in rotate sorted array II solution

---
+++ leetCode/src/081_SearchInRotatedSortedArrayIISolution/SearchInRotatedSortedArrayIISolution.cs
@@ -0,0 +1,27 @@
+﻿namespace leetCode._081_SearchInRotatedSortedArrayIISolution
+{
+    public class SearchInRotatedSortedArrayIISolution
+    {
+        public bool Search(int[] nums, int target)
+        {
+            int start = 0, end = nums.Length - 1, realStart = 0;
+
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (i > 0 && nums[i - 1] > nums[i]) realStart = i;
+            }
+
+            end = nums.Length - 1;
+            while(start <= end)
+            {
+                var mid = (start + end)/2;
+                var realMid = (realStart + mid)%nums.Length;
+                if (target == nums[realMid]) return true;
+                if (target > nums[realMid]) start = mid + 1;
+                else end = mid - 1;
+            }
+            
+            return false;
+        }
+    }
+}
---
+++ leetCode/test/SearchInRotatedSortedArrayIIFacts.cs
@@ -0,0 +1,77 @@
+﻿using leetCode._081_SearchInRotatedSortedArrayIISolution;
+using Xunit;
+using Xunit.Should;
+
+namespace leetCode.test
+{
+    public class SearchInRotatedSortedArrayIIFacts
+    {
+        private readonly SearchInRotatedSortedArrayIISolution solution = new SearchInRotatedSortedArrayIISolution();
+
+        [Fact]
+        public void should_return_false_for_empty()
+        {
+            solution.Search(new int[] {}, 0).ShouldBeFalse();
+        }
+
+        [Fact]
+        public void should_return_false_when_search_0_in_1()
+        {
+            solution.Search(new int[] {1}, 0).ShouldBeFalse();
+        }
+
+        [Fact]
+        public void should_return_true_when_search_1_in_1()
+        {
+            solution.Search(new int[] {1}, 1).ShouldBeTrue();
+        }
+
+        [Fact]
+        public void should_return_true_when_search_1_in_11()
+        {
+            solution.Search(new int[] {1, 1}, 1).ShouldBeTrue();
+        }
+
+        [Fact]
+        public void should_return_true_when_search_1_in_011()
+        {
+            solution.Search(new int[] {0, 1, 1}, 1).ShouldBeTrue();
+        }
+
+        [Fact]
+        public void should_return_true_when_search_1_in_10()
+        {
+            solution.Search(new int[] {1, 0}, 1).ShouldBeTrue();
+        }
+
+        [Fact]
+        public void should_return_true_when_search_1_in_2301()
+        {
+            solution.Search(new int[] {2, 3, 0, 1}, 1).ShouldBeTrue();
+        }
+
+        [Fact]
+        public void should_return_true_when_search_1_in_2233001()
+        {
+            solution.Search(new int[] {2, 2, 3, 3, 0, 0, 1}, 1).ShouldBeTrue();
+        }
+
+        [Fact]
+        public void should_return_false_when_search_5_in_2233001()
+        {
+            solution.Search(new int[] {2, 2, 3, 3, 0, 0, 1}, 5).ShouldBeFalse();
+        }
+
+        [Fact]
+        public void should_return_true_when_search_0_in_2202()
+        {
+            solution.Search(new int[] {2, 2, 0, 2}, 0).ShouldBeTrue();
+        }
+
+        [Fact]
+        public void should_return_true_when_search_1_in_31()
+        {
+            solution.Search(new int[] {3, 1}, 1).ShouldBeTrue();
+        }
+    }
+}
