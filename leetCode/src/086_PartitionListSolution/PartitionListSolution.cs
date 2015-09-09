967a98f050ac983f49de4893765080ae49976e0a
r00e <young_rui@hotmail.com>
2015-09-09 20:58:10 +0800

    partition list

---
+++ leetCode/src/086_PartitionListSolution/PartitionListSolution.cs
@@ -0,0 +1,37 @@
+﻿using leetCode.Common;
+
+namespace leetCode._086_PartitionListSolution
+{
+    public class PartitionListSolution
+    {
+        public ListNode Partition(ListNode head, int x)
+        {
+            var firstPart = new ListNode(0);
+            var firstProbe = firstPart;
+            var secondPart = new ListNode(0);
+            var secondProbe = secondPart;
+
+            while (head != null)
+            {
+                if (head.val < x)
+                {
+                    firstProbe.next = head;
+                    firstProbe = firstProbe.next;
+                    head = head.next;
+                    firstProbe.next = null;
+                }
+                else
+                {
+                    secondProbe.next = head;
+                    secondProbe = secondProbe.next;
+                    head = head.next;
+                    secondProbe.next = null;
+                }
+            }
+
+            firstProbe.next = secondPart.next;
+
+            return firstPart.next;
+        }
+    }
+}
---
+++ leetCode/test/PartitionListFacts.cs
@@ -0,0 +1,76 @@
+﻿using leetCode.Common;
+using leetCode._086_PartitionListSolution;
+using Xunit;
+
+namespace leetCode.test
+{
+    public class PartitionListFacts
+    {
+        private readonly PartitionListSolution solution = new PartitionListSolution();
+            
+        [Fact]
+        public void should_return_empty_for_empty_list()
+        {
+            var result = solution.Partition(null, 0);
+            CommonMethod.VerifyListResult(result, string.Empty);
+        }
+            
+        [Fact]
+        public void should_return_1_when_partition_1_with_0()
+        {
+            var result = solution.Partition(new ListNode(1), 0);
+            CommonMethod.VerifyListResult(result, "1");
+        }
+            
+        [Fact]
+        public void should_return_1_when_partition_1_with_1()
+        {
+            var result = solution.Partition(new ListNode(1), 1);
+            CommonMethod.VerifyListResult(result, "1");
+        }
+            
+        [Fact]
+        public void should_return_1_when_partition_1_with_2()
+        {
+            var result = solution.Partition(new ListNode(1), 2);
+            CommonMethod.VerifyListResult(result, "1");
+        }
+            
+        [Fact]
+        public void should_return_21_when_partition_21_with_1()
+        {
+            var result = solution.Partition(new ListNode(2){next = new ListNode(1)}, 1);
+            CommonMethod.VerifyListResult(result, "21");
+        }
+            
+        [Fact]
+        public void should_return_12_when_partition_21_with_2()
+        {
+            var result = solution.Partition(new ListNode(2){next = new ListNode(1)}, 2);
+            CommonMethod.VerifyListResult(result, "12");
+        }
+            
+        [Fact]
+        public void should_return_21_when_partition_21_with_3()
+        {
+            var result = solution.Partition(new ListNode(2){next = new ListNode(1)}, 3);
+            CommonMethod.VerifyListResult(result, "21");
+        }
+            
+        [Fact]
+        public void should_return_2113_when_partition_2131_with_3()
+        {
+            var result = solution.Partition(new ListNode(2)
+            {
+                next = new ListNode(1)
+                {
+                    next = new ListNode(3)
+                    {
+                        next = new ListNode(1)
+                    }
+                }
+            }, 3);
+            CommonMethod.VerifyListResult(result, "2113");
+        }
+    }
+}
