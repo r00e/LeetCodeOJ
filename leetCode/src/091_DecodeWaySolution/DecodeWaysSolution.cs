3c524a5b9e5c92de2785f82e814ed1b83382f3ce
r00e <young_rui@hotmail.com>
2015-09-12 16:06:31 +0800

    decode ways solution

---
+++ leetCode/src/091_DecodeWaySolution/DecodeWaysSolution.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+
+namespace leetCode._091_DecodeWaySolution
+{
+    public class DecodeWaysSolution
+    {
+        public int NumDecodings(string s)
+        {
+            var dictKey = new Collection<string>
+            {
+                "1" , "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13",
+                "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26"
+            };
+
+            var decodeWays = new int[s.Length];
+
+            for (var i = 0; i < s.Length; i++)
+            {
+                var tmp1 = 0;
+                var tmp2 = 0;
+                if (dictKey.Contains(s[i].ToString(CultureInfo.DefaultThreadCurrentCulture)))
+                {
+                    tmp1 = i == 0 ? 1 : decodeWays[i - 1];
+                }
+                if (i > 0 && dictKey.Contains(s.Substring(i - 1, 2)))
+                {
+                    tmp2 = i == 1 ? 1 : decodeWays[i - 2];
+                }
+
+                decodeWays[i] = tmp1 + tmp2;
+
+                if (decodeWays[i] == 0) return 0;
+            }
+
+            return decodeWays.LastOrDefault();
+        }
+    }
+}
---
+++ leetCode/test/DecodeWaysFacts.cs
@@ -0,0 +1,27 @@
+﻿using leetCode._091_DecodeWaySolution;
+using Xunit.Extensions;
+using Xunit.Should;
+
+namespace leetCode.test
+{
+    public class DecodeWaysFacts
+    {
+        readonly DecodeWaysSolution solution = new DecodeWaysSolution();
+
+        [Theory]
+        [InlineData("", 0)]
+        [InlineData("0", 0)]
+        [InlineData("01", 0)]
+        [InlineData("1", 1)]
+        [InlineData("9", 1)]
+        [InlineData("10", 1)]
+        [InlineData("30", 0)]
+        [InlineData("130", 0)]
+        [InlineData("1234", 3)]
+        [InlineData("1224", 5)]
+        public void should_return_decode_ways_for_input_string(string input, int decodeWays)
+        {
+            solution.NumDecodings(input).ShouldBe(decodeWays);
+        }
+    }
+}
