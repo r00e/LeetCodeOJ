c4c0a9a75b21a728c1338e571700a081829938b6
r00e <young_rui@hotmail.com>
2015-09-12 21:52:56 +0800

    reverse linked list II

---
+++ leetCode/test/ReverseLinkedListIIFacts.cs
@@ -0,0 +1,112 @@
+﻿using leetCode.Common;
+using Xunit;
+using Xunit.Should;
+
+namespace leetCode.test
+{
+    public class ReverseLinkedListIIFacts
+    {
+        private readonly ReverseLinkedListIISolution solution = new ReverseLinkedListIISolution();
+            
+        [Fact]
+        public void should_return_1_for_list_1_m_1_n_1()
+        {
+            var result = solution.ReverseBetween(new ListNode(1), 1, 1);
+
+            CommonMethod.VerifyListResult(result, "1");
+        }
+            
+        [Fact]
+        public void should_return_21_for_list_12_m_1_n_2()
+        {
+            var result = solution.ReverseBetween(new ListNode(1){next = new ListNode(2)}, 1, 2);
+
+            CommonMethod.VerifyListResult(result, "21");
+        }
+            
+        [Fact]
+        public void should_return_213_for_list_123_m_1_n_2()
+        {
+            var result = solution.ReverseBetween(
+                new ListNode(1)
+                {
+                    next = new ListNode(2)
+                    {
+                        next = new ListNode(3)
+                    }
+                }, 1, 2);
+
+            CommonMethod.VerifyListResult(result, "213");
+        }
+            
+        [Fact]
+        public void should_return_321_for_list_123_m_1_n_3()
+        {
+            var result = solution.ReverseBetween(
+                new ListNode(1)
+                {
+                    next = new ListNode(2)
+                    {
+                        next = new ListNode(3)
+                    }
+                }, 1, 3);
+
+            CommonMethod.VerifyListResult(result, "321");
+        }
+            
+        [Fact]
+        public void should_return_1324_for_list_1234_m_2_n_3()
+        {
+            var result = solution.ReverseBetween(
+                new ListNode(1)
+                {
+                    next = new ListNode(2)
+                    {
+                        next = new ListNode(3)
+                        {
+                            next = new ListNode(4)
+                        }
+                    }
+                }, 2, 3);
+
+            CommonMethod.VerifyListResult(result, "1324");
+        }
+    }
+
+    internal class ReverseLinkedListIISolution
+    {
+        public ListNode ReverseBetween(ListNode head, int m, int n)
+        {
+            if (m == n) return head;
+
+            var count = 1;
+            var probe = head;
+            var newHead = new ListNode(0);
+            var newProbe = newHead;
+
+            while (count < m)
+            {
+                newProbe.next = probe;
+                probe = probe.next;
+                newProbe = newProbe.next;
+                newProbe.next = null;
+                count++;
+            }
+
+            ListNode reverseHead = null, reverseTail = probe;
+            while (count <= n)
+            {
+                var nodeNextProbe = probe.next;
+                probe.next = reverseHead;
+                reverseHead = probe;
+                probe = nodeNextProbe;
+                count++;
+            }
+
+            reverseTail.next = probe;
+            newProbe.next = reverseHead;
+
+            return newHead.next;
+        }
+    }
+}
