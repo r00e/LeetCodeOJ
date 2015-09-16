5774e4fb9faf18dbe48516ee41470b483b1b82d8
r00e <young_rui@hotmail.com>
2015-09-16 23:38:30 +0800

    validate binary search tree

---
+++ leetCode/src/098_ValidateBinarySearchTree/ValidateBinarySearchTreeSolution.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using leetCode.Common;
+
+namespace leetCode._098_ValidateBinarySearchTree
+{
+    public class ValidateBinarySearchTreeSolution
+    {
+        public bool IsValidBST(TreeNode root)
+        {
+            if (root == null) return true;
+
+            var inOrderTraversalTreeResult = new List<int>();
+            InOrderTraversalTree(root, inOrderTraversalTreeResult);
+
+            for (var i = 0; i < inOrderTraversalTreeResult.Count - 1; i++)
+            {
+                if (inOrderTraversalTreeResult[i] >= inOrderTraversalTreeResult[i + 1]) return false;
+            }
+
+            return true;
+        }
+
+        private void InOrderTraversalTree(TreeNode treeNode, List<int> result)
+        {
+            if (treeNode == null) return;
+
+            InOrderTraversalTree(treeNode.left, result);
+            result.Add(treeNode.val);
+            InOrderTraversalTree(treeNode.right, result);
+        }
+    }
+}
---
+++ leetCode/test/ValidateBinarySearchTreeFacts.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq;
+using leetCode.Common;
+using leetCode._098_ValidateBinarySearchTree;
+using Xunit;
+using Xunit.Should;
+
+namespace leetCode.test
+{
+    public class ValidateBinarySearchTreeFacts
+    {
+        private readonly ValidateBinarySearchTreeSolution solution = new ValidateBinarySearchTreeSolution();
+            
+        [Fact]
+        public void should_return_true_for_null()
+        {
+            solution.IsValidBST(null).ShouldBeTrue();
+        }
+            
+        [Fact]
+        public void should_return_true_for_1()
+        {
+            solution.IsValidBST(new TreeNode(1)).ShouldBeTrue();
+        }
+            
+        [Fact]
+        public void should_return_false_for_11()
+        {
+            solution.IsValidBST(new TreeNode(1)
+            {
+                left = new TreeNode(1)
+            }).ShouldBeFalse();
+        }
+            
+        [Fact]
+        public void should_return_false_for_1_1()
+        {
+            solution.IsValidBST(new TreeNode(1)
+            {
+                right = new TreeNode(1)
+            }).ShouldBeFalse();
+        }
+            
+        [Fact]
+        public void should_return_true_for_213()
+        {
+            solution.IsValidBST(new TreeNode(2)
+            {
+                left = new TreeNode(1),
+                right = new TreeNode(3)
+            }).ShouldBeTrue();
+        }
+            
+        [Fact]
+        public void should_return_false_for_859__1()
+        {
+            solution.IsValidBST(new TreeNode(8)
+            {
+                left = new TreeNode(5),
+                right = new TreeNode(9) { left = new TreeNode(1)}
+            }).ShouldBeFalse();
+        }
+    }
+}
