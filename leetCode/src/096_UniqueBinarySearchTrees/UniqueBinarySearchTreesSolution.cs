e5bb324753a46e155d0ce00f25684a892d4e0a9a
r00e <young_rui@hotmail.com>
2015-09-16 22:04:20 +0800

    unique binary search trees

---
+++ leetCode/src/096_UniqueBinarySearchTrees/UniqueBinarySearchTreesSolution.cs
@@ -0,0 +1,26 @@
+﻿namespace leetCode._096_UniqueBinarySearchTrees
+{
+    public class UniqueBinarySearchTreesSolution
+    {
+        public int NumTrees(int n)
+        {
+            if (n < 1) return 0;
+
+            var result = new int[n+1];
+            result[0] = 1;
+
+            for (var len = 1; len <= n; len++)
+            {
+                for (var i = 0; i < len; i++)
+                {
+                    var leftCount = result[i];
+                    var rightCount = result[len - 1 - i];
+
+                    result[len] += leftCount*rightCount;
+                }
+            }
+
+            return result[n];
+        }
+    }
+}
---
+++ leetCode/test/UniqueBinarySearchTreesFacts.cs
@@ -0,0 +1,21 @@
+﻿using leetCode._096_UniqueBinarySearchTrees;
+using Xunit.Extensions;
+using Xunit.Should;
+
+namespace leetCode.test
+{
+    public class UniqueBinarySearchTreesFacts
+    {
+        private readonly UniqueBinarySearchTreesSolution solution = new UniqueBinarySearchTreesSolution();
+            
+        [Theory]
+        [InlineData(0, 0)]
+        [InlineData(1, 1)]
+        [InlineData(2, 2)]
+        [InlineData(3, 5)]
+        public void should_return_BST_trees_number_for_given_number(int givenNumber, int treesNumber)
+        {
+            solution.NumTrees(givenNumber).ShouldBe(treesNumber);
+        }
+    }
+}
