a29be39ce37e73791e720debe6b7e27faafb98e6
r00e <young_rui@hotmail.com>
2015-09-17 22:24:14 +0800

    Binary Tree Zigzag Level Order Traversal Solution

---
+++ leetCode/src/103_BinaryTreeZigzagLevelOrderTraversalSolution/BinaryTreeZigzagLevelOrderTraversalSolution.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using leetCode.Common;
+
+namespace leetCode._103_BinaryTreeZigzagLevelOrderTraversalSolution
+{
+    public class BinaryTreeZigzagLevelOrderTraversalSolution
+    {
+        private enum Sequence
+        {
+            LeftToRight,
+            RightToLeft
+        };
+
+        public IList<IList<int>> ZigzagLevelOrder(TreeNode root)
+        {
+            var result = new List<IList<int>>();
+            if (root == null) return result;
+
+            var sequence = Sequence.RightToLeft;
+            var leftToRightLayer = new Stack<TreeNode>();
+            leftToRightLayer.Push(root);
+            var rightToLeftLayer = new Stack<TreeNode>();
+        
+            while (leftToRightLayer.Count != 0 || rightToLeftLayer.Count != 0)
+            {
+                if (sequence == Sequence.RightToLeft)
+                {
+                    var list = new List<int>();
+                    while (leftToRightLayer.Count != 0)
+                    {
+                        var node = leftToRightLayer.Pop();
+                        if (node != null)
+                        {
+                            list.Add(node.val);
+                            rightToLeftLayer.Push(node.left);
+                            rightToLeftLayer.Push(node.right);    
+                        }
+                    }
+                    if (list.Count != 0) result.Add(list);
+                    sequence = Sequence.LeftToRight;
+                }
+                if (sequence == Sequence.LeftToRight)
+                {
+                    var list = new List<int>();
+                    while (rightToLeftLayer.Count != 0)
+                    {
+                        var node = rightToLeftLayer.Pop();
+                        if (node != null)
+                        {
+                            list.Add(node.val);
+                            leftToRightLayer.Push(node.right);
+                            leftToRightLayer.Push(node.left);
+                        }
+                    }
+                    if (list.Count != 0) result.Add(list);
+                    sequence = Sequence.RightToLeft;
+                }
+            }
+        
+            return result;
+        }
+    }
+}
---
+++ leetCode/test/BinaryTreeZigzagLevelOrderTraversalFacts.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using leetCode.Common;
+using leetCode._103_BinaryTreeZigzagLevelOrderTraversalSolution;
+using Xunit;
+using Xunit.Should;
+
+namespace leetCode.test
+{
+    public class BinaryTreeZigzagLevelOrderTraversalFacts
+    {
+        private readonly BinaryTreeZigzagLevelOrderTraversalSolution solution =
+            new BinaryTreeZigzagLevelOrderTraversalSolution();
+
+        [Fact]
+        public void should_return_empty_for_null()
+        {
+            solution.ZigzagLevelOrder(null).ShouldBe(new List<IList<int>>());
+        }
+
+        [Fact]
+        public void should_return_result_for_1()
+        {
+            solution.ZigzagLevelOrder(new TreeNode(1)).ShouldBe(new List<IList<int>> {new List<int> {1}});
+        }
+
+        [Fact]
+        public void should_return_result_for_12()
+        {
+            solution.ZigzagLevelOrder(new TreeNode(1)
+            {
+                left = new TreeNode(2)
+            }).ShouldBe(new List<IList<int>>
+            {
+                new List<int> {1},
+                new List<int> {2}
+            });
+        }
+
+        [Fact]
+        public void should_return_result_for_123()
+        {
+            solution.ZigzagLevelOrder(new TreeNode(1)
+            {
+                left = new TreeNode(2),
+                right = new TreeNode(3)
+            }).ShouldBe(new List<IList<int>>
+            {
+                new List<int> {1},
+                new List<int> {3, 2}
+            });
+        }
+    }
+}
