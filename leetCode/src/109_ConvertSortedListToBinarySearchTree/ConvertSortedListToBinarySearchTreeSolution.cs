fa528c34dd7911aabc479f731813950088e58c65
r00e <young_rui@hotmail.com>
2015-09-20 14:07:42 +0800

    convert sorted list to binary search tree

---
+++ leetCode/src/109_ConvertSortedListToBinarySearchTree/ConvertSortedListToBinarySearchTreeSolution.cs
@@ -0,0 +1,41 @@
+﻿using leetCode.Common;
+
+namespace leetCode._109_ConvertSortedListToBinarySearchTree
+{
+    public class ConvertSortedListToBinarySearchTreeSolution
+    {
+        private ListNode list;
+
+        public TreeNode SortedListToBST(ListNode head)
+        {
+            if(head == null) return null;
+
+            list = head;
+
+            var length = 0;
+            var probe = head;
+            while (probe != null)
+            {
+                length++;
+                probe = probe.next;
+            }
+
+            return Helper(0, length - 1);
+        }
+
+        private TreeNode Helper(int start, int end)
+        {
+            if (start > end) return null;
+
+            var mid = (start + end)/2;
+            var tree = new TreeNode(0);
+
+            tree.left = Helper(start, mid - 1);
+            tree.val = list.val;
+            list = list.next;
+            tree.right = Helper(mid + 1, end);
+
+            return tree;
+        }
+    }
+}
---
+++ leetCode/test/ConvertSortedListToBinarySearchTreeFacts.cs
@@ -0,0 +1,44 @@
+﻿using leetCode.Common;
+using leetCode._109_ConvertSortedListToBinarySearchTree;
+using Xunit;
+
+namespace leetCode.test
+{
+    public class ConvertSortedListToBinarySearchTreeFacts
+    {
+        private readonly ConvertSortedListToBinarySearchTreeSolution solution =
+            new ConvertSortedListToBinarySearchTreeSolution();
+
+        [Fact]
+        public void should_return_empty_for_null()
+        {
+            var result = solution.SortedListToBST(null);
+
+            CommonMethod.VerifyTreeNodeResult(result, "[]");
+        }
+
+        [Fact]
+        public void should_return_result_for_1()
+        {
+            var result = solution.SortedListToBST(new ListNode(1));
+
+            CommonMethod.VerifyTreeNodeResult(result, "[1]");
+        }
+
+        [Fact]
+        public void should_return_result_for_12()
+        {
+            var result = solution.SortedListToBST(new ListNode(1){next = new ListNode(2)});
+
+            CommonMethod.VerifyTreeNodeResult(result, "[1, #, 2]");
+        }
+
+        [Fact]
+        public void should_return_result_for_123()
+        {
+            var result = solution.SortedListToBST(new ListNode(1){next = new ListNode(2) {next = new ListNode(3)}});
+
+            CommonMethod.VerifyTreeNodeResult(result, "[2, 1, 3]");
+        }
+    }
+}
