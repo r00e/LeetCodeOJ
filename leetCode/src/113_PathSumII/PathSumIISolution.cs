f5cd4dec2eea62f1dec286ef1cd4d69377d80018
r00e <young_rui@hotmail.com>
2015-09-20 21:38:13 +0800

    path sum II

---
+++ leetCode/src/113_PathSumII/PathSumIISolution.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using leetCode.Common;
+
+namespace leetCode._113_PathSumII
+{
+    public class PathSumIISolution
+    {
+        readonly List<IList<int>> results = new List<IList<int>>();
+
+        public IList<IList<int>> PathSum(TreeNode root, int sum)
+        {
+            if (root == null) return results;
+
+            var result = new List<int>();
+            Helper(root, result);
+
+            return results.Where(r => r.Sum() == sum).ToList();
+        }
+
+        private void Helper(TreeNode root, List<int> result)
+        {
+            if (root == null) return;
+
+            var resultCopy = new List<int>();
+            result.ForEach(resultCopy.Add);
+
+            if (root.left == null && root.right == null)
+            {
+                resultCopy.Add(root.val);
+                results.Add(resultCopy);
+            }
+            else
+            {
+                resultCopy.Add(root.val);
+                Helper(root.left, resultCopy);
+                Helper(root.right, resultCopy);
+            }
+        }
+    }
+}
---
+++ leetCode/test/PathSumIIFacts.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using leetCode.Common;
+using leetCode._113_PathSumII;
+using Xunit;
+using Xunit.Should;
+
+namespace leetCode.test
+{
+    public class PathSumIIFacts
+    {
+        private readonly PathSumIISolution solution = new PathSumIISolution();
+
+        [Fact]
+        public void should_return_empty_for_null()
+        {
+            solution.PathSum(null, 0).ShouldBe(new List<IList<int>>());
+        }
+
+        [Fact]
+        public void should_return_result_for_tree_1_and_sum_0()
+        {
+            solution.PathSum(new TreeNode(1), 0).ShouldBe(new List<IList<int>>());
+        }
+
+        [Fact]
+        public void should_return_result_for_tree_1_and_sum_1()
+        {
+            solution.PathSum(new TreeNode(1), 1).ShouldBe(new List<IList<int>> {new List<int> {1}});
+        }
+
+        [Fact]
+        public void should_return_result_for_tree_122_and_sum_3()
+        {
+            solution.PathSum(
+                new TreeNode(1)
+                {
+                    left = new TreeNode(2),
+                    right = new TreeNode(2)
+                }, 3
+            ).ShouldBe(new List<IList<int>>
+            {
+                new List<int> {1, 2},
+                new List<int> {1, 2}
+            });
+        }
+
+        [Fact]
+        public void should_return_result_for_tree_132_and_sum_3()
+        {
+            solution.PathSum(
+                new TreeNode(1)
+                {
+                    left = new TreeNode(3),
+                    right = new TreeNode(2)
+                }, 3
+            ).ShouldBe(new List<IList<int>>
+            {
+                new List<int> {1, 2}
+            });
+        }
+
+        [Fact]
+        public void should_return_result_for_tree_m1_m2_and_sum_m3()
+        {
+            solution.PathSum(
+                new TreeNode(-1)
+                {
+                    right = new TreeNode(-2)
+                }, -3
+            ).ShouldBe(new List<IList<int>>
+            {
+                new List<int> {-1, -2}
+            });
+        }
+    }
+}
