57f5b350b1e5b19c6768a9f48bb8d46f9198da49
r00e <young_rui@hotmail.com>
2015-09-21 00:04:24 +0800

    triangle

---
+++ leetCode/src/120_Triangle/TriangleSolution.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace leetCode._120_Triangle
+{
+    public class TriangleSolution
+    {
+        public int MinimumTotal(IList<IList<int>> triangle)
+        {
+            var results = new List<IList<int>>
+            {
+                new List<int>
+                {
+                    triangle.First().First()
+                }
+            };
+
+            for (var triangleRow = 1; triangleRow < triangle.Count; triangleRow++)
+            {
+                var result = new List<int>();
+
+                for (var triangleColumn = 0; triangleColumn <= triangleRow; triangleColumn++)
+                {
+                    if (triangleColumn == 0 || triangleColumn == triangleRow)
+                    {
+                        result.Add(results[triangleRow - 1][triangleColumn == 0 ? 0 : triangleColumn - 1]
+                                   + triangle[triangleRow][triangleColumn]);
+                    }
+                    else
+                    {
+                        result.Add(
+                            Math.Min(results[triangleRow - 1][triangleColumn - 1],
+                                results[triangleRow - 1][triangleColumn]) + triangle[triangleRow][triangleColumn]);
+                    }
+                }
+
+                results.Add(result);
+            }
+
+            return results.Last().Min();
+        }
+    }
+}
---
+++ leetCode/test/TriangleFacts.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using leetCode._120_Triangle;
+using Xunit;
+using Xunit.Should;
+
+namespace leetCode.test
+{
+    public class TriangleFacts
+    {
+        private readonly TriangleSolution solution = new TriangleSolution();
+
+        [Fact]
+        public void should_return_0_for_triangle_0()
+        {
+            solution.MinimumTotal(new List<IList<int>> {new List<int> {0}}).ShouldBe(0);
+        }
+
+        [Fact]
+        public void should_return_2_for_triangle_1_11()
+        {
+            solution.MinimumTotal(new List<IList<int>> {new List<int> {1}, new List<int> {1, 1}}).ShouldBe(2);
+        }
+
+        [Fact]
+        public void should_return_1_for_triangle_1_10()
+        {
+            solution.MinimumTotal(new List<IList<int>> {new List<int> {1}, new List<int> {1, 0}}).ShouldBe(1);
+        }
+
+        [Fact]
+        public void should_return_4_for_triangle_1_23_930()
+        {
+            solution.MinimumTotal(new List<IList<int>>
+            {
+                new List<int> {1},
+                new List<int> {2, 3},
+                new List<int> {9, 3, 0}
+            }).ShouldBe(4);
+        }
+
+        [Fact]
+        public void should_return_14_for_triangle_2_34_659_4480()
+        {
+            solution.MinimumTotal(new List<IList<int>>
+            {
+                new List<int> {2},
+                new List<int> {3, 4},
+                new List<int> {6, 5, 9},
+                new List<int> {4, 4, 8, 0}
+            }).ShouldBe(14);
+        }
+    }
+}
