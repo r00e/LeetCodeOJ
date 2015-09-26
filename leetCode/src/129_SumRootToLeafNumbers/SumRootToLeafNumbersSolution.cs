78443c9b683e085d1b3b738eabb11b0962b3c1ee
r00e <young_rui@hotmail.com>
2015-09-26 12:10:05 +0800

    sum root to leaf numbers

---
+++ leetCode/src/129_SumRootToLeafNumbers/SumRootToLeafNumbersSolution.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using leetCode.Common;
+
+namespace leetCode._129_SumRootToLeafNumbers
+{
+    public class SumRootToLeafNumbersSolution
+    {
+        public int SumNumbers(TreeNode root)
+        {
+            if (root == null) return 0;
+            var allLeavesSum = 0;
+
+            var stack = new Stack<TreeNode>();
+            stack.Push(root);
+
+            while (stack.Count != 0)
+            {
+                var node = stack.Pop();
+
+                if (node.left != null)
+                {
+                    node.left.val += node.val*10;
+                    stack.Push(node.left);
+                }
+                if (node.right != null)
+                {
+                    node.right.val += node.val*10;
+                    stack.Push(node.right);
+                }
+
+                allLeavesSum += IsLeafNode(node) ? node.val : 0;
+            }
+
+            return allLeavesSum;
+        }
+
+        private static bool IsLeafNode(TreeNode node)
+        {
+            return node.left == null && node.right == null;
+        }
+    }
+}
---
+++ leetCode/test/SumRootToLeafNumbersFacts.cs
@@ -0,0 +1,72 @@
+﻿using leetCode.Common;
+using leetCode._129_SumRootToLeafNumbers;
+using Xunit;
+using Xunit.Should;
+
+namespace leetCode.test
+{
+    public class SumRootToLeafNumbersFacts
+    {
+        private readonly SumRootToLeafNumbersSolution solution = new SumRootToLeafNumbersSolution();
+
+        [Fact]
+        public void should_return_0_for_null()
+        {
+            solution.SumNumbers(null).ShouldBe(0);
+        }
+
+        [Fact]
+        public void should_return_1_for_1()
+        {
+            solution.SumNumbers(new TreeNode(1)).ShouldBe(1);
+        }
+
+        [Fact]
+        public void should_return_12_for_1_2()
+        {
+            solution.SumNumbers(
+                new TreeNode(1)
+                {
+                    left = new TreeNode(2)
+                }).ShouldBe(12);
+        }
+
+        [Fact]
+        public void should_return_12_for_1__2()
+        {
+            solution.SumNumbers(
+                new TreeNode(1)
+                {
+                    right = new TreeNode(2)
+                }).ShouldBe(12);
+        }
+
+        [Fact]
+        public void should_return_25_for_1_23()
+        {
+            solution.SumNumbers(
+                new TreeNode(1)
+                {
+                    left = new TreeNode(2),
+                    right = new TreeNode(3)
+                }).ShouldBe(25);
+        }
+
+        [Fact]
+        public void should_return_259_for_1_23_4__5()
+        {
+            solution.SumNumbers(
+                new TreeNode(1)
+                {
+                    left = new TreeNode(2)
+                    {
+                        left = new TreeNode(4)
+                    },
+                    right = new TreeNode(3)
+                    {
+                        right = new TreeNode(5)
+                    }
+                }).ShouldBe(259);
+        }
+    }
+}
